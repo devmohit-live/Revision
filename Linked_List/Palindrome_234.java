3b5253fce7281cdc537edaca869bb6b527473a42
Mohit <mohitsingh.it5@gmail.com>
2021-06-15 00:58:11 +0530

    Palindorme_234- corrected given list manipulation | Mohit

--- Linked_List/Palindrome_234.java
+++ Linked_List/Palindrome_234.java
@@ -2,27 +2,34 @@ package Linked_List;
 
 class Palindorme_234 {
     public boolean isPalindrome(ListNode head) {
-
         if (head == null || head.next == null)
             return true;
 
         ListNode mid = middleNode(head);
+        ListNode newHead = mid.next;
+        mid.next = null;
 
         ListNode p1 = head;
-        ListNode p2 = mid.next;
-
-        mid.next = null;
+        ListNode p2 = newHead;
 
         p2 = reverse(p2);
 
+        boolean ans = true;
+
         while (p1 != null && p2 != null) {
-            if (p1.val != p2.val)
-                return false;
+            if (p1.val != p2.val) {
+                // as we have to correct the ll -> undo the changes we made to the list
+                ans = false;
+                break;
+            }
             p1 = p1.next;
             p2 = p2.next;
         }
 
-        return true;
+        // undo changes
+        newHead = reverse(newHead);
+        mid.next = newHead;
+        return ans;
     }
 
     private ListNode middleNode(ListNode head) {
@@ -54,5 +61,4 @@ class Palindorme_234 {
         }
         return prev;
     }
-
 }
