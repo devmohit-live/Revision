5953acd2e975b56e81e15e741b1e5548028b2753
Mohit <mohitsingh.it5@gmail.com>
2021-07-03 11:15:24 +0530

    Second Approach of LevelOrderZigZag of GT and BT  Mohit

--- Graph/Tree_LevelOrder/ZigZagOfBT.java
+++ Graph/Tree_LevelOrder/ZigZagOfBT.java
@@ -1,4 +1,29 @@
+package Tree_LevelOrder;
+
+import java.util.*;
+
 public class ZigZagOfBT {
+
+    public class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
     // Leetcode 103
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
 
@@ -32,6 +57,60 @@ public class ZigZagOfBT {
 
         return res;
     }
-}
 
-// Different Way (Class):
+    // Different Way (Class): Using LinkedList for swapping st and q
+
+    // add children to stack and after adding all children of a node swap the ref of
+    // q and st
+
+    // addition of children will be according to level =>
+
+    // level ==even => left to right
+    // level -- odd => right to left
+
+    // (same approach used for generic tree)
+
+    public List<List<Integer>> zigzagLevelOrder2(TreeNode root) {
+
+        int lv = 0;
+        LinkedList<TreeNode> q = new LinkedList<>();
+        LinkedList<TreeNode> st = new LinkedList<>();
+        q.addLast(root);
+        List<List<Integer>> res = new ArrayList<>();
+
+        if (root == null)
+            return res;
+
+        while (q.size() > 0) {
+            List<Integer> smallAns = new ArrayList<>();
+            int curr = q.size();
+            while (curr-- > 0) {
+                TreeNode rm = q.removeFirst();
+                smallAns.add(rm.val);
+
+                if (lv % 2 == 0) { // add left then right
+                    if (rm.left != null)
+                        st.addFirst(rm.left);
+                    if (rm.right != null)
+                        st.addFirst(rm.right);
+                } else {
+                    // add right then left
+                    if (rm.right != null)
+                        st.addFirst(rm.right);
+                    if (rm.left != null)
+                        st.addFirst(rm.left);
+                }
+
+            }
+
+            LinkedList<TreeNode> tmp = q;
+            q = st;
+            st = tmp;
+            res.add(smallAns);
+            lv++;
+
+        }
+        return res;
+    }
+
+}
--- Graph/Tree_LevelOrder/ZigZagOfGenericTree.java
+++ Graph/Tree_LevelOrder/ZigZagOfGenericTree.java
@@ -1,56 +1,104 @@
-class ZigZagOfGenericTree{
-    //portal
-      public static void levelOrderLinewiseZZ(Node node){
-      int lv=0;
-      LinkedList<Node>q =new LinkedList<>();
-      q.addLast(node);
-      
-      while(q.size()>0){
-        int curr= q.size();
-        ArrayList<Integer> prints=new ArrayList<>();
-        
-        while(curr-->0){
-            Node rm = q.removeFirst();
-            prints.add(rm.data);
-            for(Node child: rm.children) q.addLast(child);
-            
-        }
-        if((lv&1)==1){
-            Collections.reverse(prints);
-        }
-        for(int item : prints) System.out.print(item+" ");
-        
-        System.out.println();lv++;
-        
-        
-     /*old : Wrong bcz at level=2 and onwards the addition order of children was disturbed for further children will print in different orders
-        while(curr-->0){
-          Node rm = q.removeFirst();
-          
-          System.out.print(rm.data+" ");
-          
-         if((lv&1)==1){
-                for(Node child: rm.children) q.add(child);
-         }else{
-         
-              for(int i=rm.children.size()-1;i>=0;i--){
-                 q.add(rm.children.get(i) );
-             }
-             
-         }
-          
-        }
-       System.out.println(); lv++;
-     */
-        
+package Tree_LevelOrder;
+
+import java.util.*;
+
+class ZigZagOfGenericTree {
+
+  // portal
+  public static void levelOrderLinewiseZZ(Node node) {
+    int lv = 0;
+    LinkedList<Node> q = new LinkedList<>();
+    q.addLast(node);
+
+    while (q.size() > 0) {
+      int curr = q.size();
+      ArrayList<Integer> prints = new ArrayList<>();
+
+      while (curr-- > 0) {
+        Node rm = q.removeFirst();
+        prints.add(rm.data);
+        for (Node child : rm.children)
+          q.addLast(child);
+
       }
-      
+      if ((lv & 1) == 1) {
+        // o(n) in reverse
+        Collections.reverse(prints);
+      }
+      for (int item : prints)
+        System.out.print(item + " ");
+
+      System.out.println();
+      lv++;
+
+      /*
+       * old : Wrong bcz at level=2 and onwards the addition order of children was
+       * disturbed for further children will print in different orders => that's why
+       * we are using stack and q in econd approach while(curr-->0){ Node rm =
+       * q.removeFirst();
+       * 
+       * System.out.print(rm.data+" ");
+       * 
+       * if((lv&1)==1){ for(Node child: rm.children) q.add(child); }else{
+       * 
+       * for(int i=rm.children.size()-1;i>=0;i--){ q.add(rm.children.get(i) ); }
+       * 
+       * }
+       * 
+       * } System.out.println(); lv++;
+       */
+
+    }
+
   }
 
+  // Different Way (Class): Using LinkedList for swapping st and q
 
-  //Different Way (Class):
+  // add children to stack and after adding all children of a node swap the ref of
+  // q and st
 
+  // addition of children will be according to level =>
 
-  
+  // level ==even => left to right
+  // level -- odd => right to left
+
+  // (same approach used for generic tree)
+
+  public static void levelOrderLinewiseZZ2(Node node) {
+    int lv = 0;
+    LinkedList<Node> q = new LinkedList<>();
+    LinkedList<Node> st = new LinkedList<>(); // addfirst, removefirst
+    q.addLast(node);
+
+    while (q.size() > 0) {
+      int curr = q.size();
+
+      while (curr-- > 0) {
+        Node rm = q.removeFirst();
+        System.out.print(rm.data + " ");
+        if (lv % 2 == 0) {
+          for (Node child : rm.children)
+            st.addFirst(child);
+        } else {
+          for (int i = rm.children.size() - 1; i >= 0; i--) {
+            // adding in rev order
+            Node child = rm.children.get(i);
+            st.addFirst(child);
+          }
+        }
+
+      }
+      // /swapping refrence
+
+      LinkedList<Node> tmp = q;
+      q = st;
+      st = tmp;
+
+      System.out.println();
+      lv++;
+
+    }
+
+  }
 
 }
