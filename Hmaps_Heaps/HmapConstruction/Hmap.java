27a4a7668657fac7d8657ba1425467b4f6947d11
Mohit <mohitsingh.it5@gmail.com>
2021-07-15 11:21:27 +0530

    HashMap implemeted | Mohit

--- Hmaps_Heaps/HmapConstruction/Hmap.java
+++ Hmaps_Heaps/HmapConstruction/Hmap.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class Hmap {
@@ -37,40 +38,68 @@ public class Hmap {
 
     // Basic
     // Functions================================================================
+    // No need to search for item in get, remove manually as containskey will
+    // autoatically move that item to start(if exists)
 
-    public Integer get(Integer key) {
-        LinkedList<Node> group = getGroup(key);
-        int groupSize = group.size();
-        Integer ans = null;
+    // not needed
+    // public Integer get(Integer key) {
+    // LinkedList<Node> group = getGroup(key);
+    // int groupSize = group.size();
+    // Integer ans = null;
 
-        while (groupSize-- > 0) {
-            Node rm = group.removeFirst();
-            group.addLast(rm);
+    // while (groupSize-- > 0) {
+    // Node rm = group.removeFirst();
+    // group.addLast(rm);
 
-            if (rm.key == key) {
-                ans = rm.value;
-                break;
-            }
-        }
-        return ans;
+    // if (rm.key == key) {
+    // ans = rm.value;
+    // break;
+    // }
+    // }
+    // return ans;
+    // }
+
+    public int size() {
+        return this.noOfEl;
     }
 
+    public boolean isEmpty() {
+        return this.noOfEl == 0;
+    }
+
+    //
+
+    // // Always use getFirst and then iff necessary use removefirst as removeFirst
+    // when rm.key==key disturb the arrangement for other fuctions like remove,get
+
     public boolean containsKey(Integer key) {
+        /**
+         * in this process rearrangement of items happens and item to be searched will
+         * come at 0th index
+         */
+
         LinkedList<Node> group = getGroup(key);
-        int groupSize = group.size();
-        boolean found = false;
+        int gs = group.size();
+        boolean res = false;
+        while (gs-- > 0) {
+            // Node rm = (Node) group.removeFirst();
+            // group.addLast(rm);
 
-        while (groupSize-- > 0) {
-            Node rm = (Node) group.removeFirst();
-            group.addLast(rm);
-
-            if (rm.key == key) {
-                found = true;
+            if (group.getFirst().key.equals(key)) {
+                res = true;
                 break;
             }
 
+            group.addLast(group.removeFirst());
         }
-        return found;
+
+        return res;
+    }
+
+    public Integer get(Integer key) {
+        boolean present = containsKey(key);
+        LinkedList<Node> group = getGroup(key);
+        return present ? group.getFirst().value : null;
     }
 
     public Integer getOrDefault(Integer key, Integer defaultValue) {
@@ -78,29 +107,89 @@ public class Hmap {
         return ans == null ? defaultValue : ans;
     }
 
-    public void put(Integer key, Integer val) {
+    public Integer put(Integer key, Integer val) {
+        // check of key already present -> update
+        boolean present = containsKey(key);
         LinkedList<Node> group = getGroup(key);
-        group.addLast(new Node(key, val));
+        if (present) {
+            Integer old = group.getFirst().value;
+            group.getFirst().value = val;
+            return old;
+        } else {
+            group.addLast(new Node(key, val));
+            this.noOfEl++;
+            double lf = (group.size() * 1.0) / this.bucketLen;
+            if (lf > 0.4)
+                reHash();
 
-    }
-
-    public Integer remove(Integer key) throws Exception {
-        keyExisist(key);
-        LinkedList<Node> group = getGroup(key);
-        int groupSize = group.size();
-        while (groupSize-- > 0) {
-            Node rm = (Node) group.removeFirst();
-
-            if (rm.key == key) {
-                return rm.value;
-            } else {
-                group.addLast(rm);
-            }
         }
         return null;
     }
 
-    // Other
+    public Integer putIfAbsent(Integer key, Integer defaultValue) {
+        boolean present = containsKey(key);
+        LinkedList<Node> group = getGroup(key);
+
+        if (present) {
+            return group.getFirst().value;
+        }
+        group.addLast(new Node(key, defaultValue));
+        this.noOfEl++;
+        double lf = (group.size() * 1.0) / this.bucketLen;
+        if (lf > 0.4)
+            reHash();
+        return null;
+    }
+
+    public Integer remove(Integer key) {
+        boolean res = containsKey(key);
+        LinkedList<Node> group = getGroup(key);
+
+        if (res) {
+            this.noOfEl--;
+            return group.removeFirst().value;
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("{");
+        int tempSize = this.noOfEl;
+        for (int i = 0; i < this.bucketLen; i++) {
+            LinkedList<Node> group = this.buckets[i];
+            int size = group.size();
+            while (size-- > 0) {
+
+                Node node = group.removeFirst();
+                sb.append(node.key + "=" + node.value);
+                group.addLast(node);
+
+                if (--tempSize != 0)
+                    sb.append(", ");
+            }
+        }
+        sb.append("}");
+        return sb.toString();
+    }
+
+    public ArrayList<Integer> keySet() {
+        ArrayList<Integer> keys = new ArrayList<>();
+        for (int i = 0; i < this.bucketLen; i++) {
+            LinkedList<Node> group = this.buckets[i];
+            int n = group.size();
+            while (n-- > 0) {
+                Node rm = group.removeFirst();
+                keys.add(rm.key);
+                group.addLast(rm);
+            }
+        }
+        return keys;
+    }
+
+    //
+    // DS dependencies
     // functions================================================================
 
     private int getHashCode(Integer key) {
@@ -112,10 +201,28 @@ public class Hmap {
         return this.buckets[hc];
     }
 
-    // Exceptions ==========================================================
-    private void keyExisist(Integer key) throws Exception {
-        if (!containsKey(key))
-            throw new Exception("Key you are trying to remove the key that doesn't exixts ");
+    private void reHash() {
+
+        // System.out.println("Rehasing is Done");
+        // load factor = 0.4
+        // increasing factor = 1.5
+        LinkedList[] oldbucket = this.buckets;
+        int oldsize = oldbucket.length;
+
+        initialize((int) (1.5 * oldsize));
+
+        for (int i = 0; i < oldsize; i++) {
+            LinkedList<Node> group = oldbucket[i];
+            int groupSize = group.size();
+            while (groupSize-- > 0) {
+                Node rm = group.removeFirst();
+                put(rm.key, rm.value);
+            }
+
+        }
+
     }
 
+    // Exceptions ==========================================================
+    // Doesn't gives exception on remove and get -> gives null
 }
--- Hmaps_Heaps/HmapConstruction/client.java
+++ Hmaps_Heaps/HmapConstruction/client.java
@@ -1,13 +1,37 @@
 public class client {
     public static void test() throws Exception {
-        Hmap map = new Hmap(100);
-        for (int i = 1; i <= 100; i++) {
+        // Hmap map = new Hmap(100);
+        // default size = 16
+        Hmap map = new Hmap(10);
+        // HashMap map = new HashMap();
+        for (int i = 1; i <= 10; i++) {
             map.put(i, i * 10);
         }
 
+        System.out.println("Size is " + map.size());
         System.out.println(map.containsKey(10));
         System.out.println(map.remove(20));
-        map.remove(-1);// gives exception
+        System.out.println(map.remove(2));
+        System.out.println(map.remove(1));
+        System.out.println(map.remove(-1));
+        System.out.println("Size is " + map.size());
+        System.out.println(map.getOrDefault(-16, -15));
+        System.out.println(map.putIfAbsent(-8, 100));
+        System.out.println(map.putIfAbsent(-8, 110));
+        System.out.println(map);
+        System.out.println(map.keySet());
+        System.out.println(map.remove(-8));
+
+        // Checking Rehasing
+        map.put(11, 0);
+        map.put(21, 0);
+        map.put(31, 0);
+        map.put(41, 0);
+        map.put(51, 0);
+        map.put(61, 0);
+
+        System.out.println(map);
+
     }
 
     public static void main(String[] args) throws Exception {
