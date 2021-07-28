595cf235068d9711b0e57c70b74479d1a67476df
Mohit <mohitsingh.it5@gmail.com>
2021-07-28 20:28:28 +0530

    nqueen isSafe optimised comb and perm | Mohit

--- Recursion/nQueenSet.java
+++ Recursion/nQueenSet.java
@@ -108,22 +108,124 @@ public class nQueenSet {
         return count;
     }
 
-    public static void nqueen() {
+    public static int nQueenPermuSubseq(boolean[][] box, int tnq, String asf, int idx, int m, int n) {
+        if (tnq == 0 || idx == n * m) {
+            if (tnq == 0) {
+                System.out.println(asf);
+                return 1;
+            }
+            return 0;
+        }
+
+        int count = 0;
+
+        int r = idx / m;
+        int c = idx % m;
+
+        if (isSafeToPlaceQueenFullDirection(box, r, c) && !box[r][c]) {
+            box[r][c] = true;
+            count += nQueenPermuSubseq(box, tnq - 1, asf + "(" + r + "," + c + ") ", 0, m, n);
+            box[r][c] = false;
+        }
+        count += nQueenPermuSubseq(box, tnq, asf, idx + 1, m, n);
+
+        return count;
+    }
+
+    // ISSAFE OPTIMISED
+
+    static boolean[] row, col, diag, antidiag;
+
+    public static int nQueen_02(int n, int m, int tnq, String asf, int idx) {
+        if (tnq == 0) {
+            System.out.println(asf);
+            return 1;
+        }
+
+        int count = 0;
+        for (int b = idx; b < n * m; b++) {
+            int r = b / m;
+            int c = b % m;
+
+            if (!row[r] && !col[c] && !diag[r + c] && !antidiag[r - c + m - 1]) {
+                row[r] = col[c] = diag[r + c] = antidiag[r - c + m - 1] = true;
+                count += nQueen_02(m, n, tnq - 1, asf + "(" + r + "," + c + ") ", b + 1);
+                row[r] = col[c] = diag[r + c] = antidiag[r - c + m - 1] = false;
+            }
+        }
+        return count;
+    }
+
+    public static boolean nQueen_02_firstWay(int n, int m, int tnq, String asf, int idx) {
+        if (tnq == 0) {
+            System.out.println(asf);
+            return true;
+        }
+
+        boolean res = false;
+        for (int b = idx; b < n * m; b++) {
+            int r = b / m;
+            int c = b % m;
+
+            if (!row[r] && !col[c] && !diag[r + c] && !antidiag[r - c + m - 1]) {
+                row[r] = col[c] = diag[r + c] = antidiag[r - c + m - 1] = true;
+                res = res || nQueen_02_firstWay(m, n, tnq - 1, asf + "(" + r + "," + c + ") ", b + 1);
+                row[r] = col[c] = diag[r + c] = antidiag[r - c + m - 1] = false;
+            }
+        }
+        return res;
+    }
+
+    public static int nQueen_02Perm(int n, int m, int tnq, String asf, int idx) {
+        if (tnq == 0) {
+            System.out.println(asf);
+            return 1;
+        }
+
+        int count = 0;
+        for (int b = 0; b < n * m; b++) {
+            int r = b / m;
+            int c = b % m;
+
+            if (!row[r] && !col[c] && !diag[r + c] && !antidiag[r - c + m - 1]) {
+                row[r] = col[c] = diag[r + c] = antidiag[r - c + m - 1] = true;
+                count += nQueen_02Perm(m, n, tnq - 1, asf + "(" + r + "," + c + ") ", b + 1);
+                row[r] = col[c] = diag[r + c] = antidiag[r - c + m - 1] = false;
+            }
+        }
+        return count;
+    }
+
+    // DRIVERS AND MAIN
+    public static void nqueenCombs() {
         int n = 4, tnq = 4;
         boolean[][] box = new boolean[n][n];
         System.out.println("NQueen comb ncr " + nQueen_01(box, 0, tnq, ""));
         System.out.println("NQueen comb Sunseq " + nQueen_01Subseq(box, 4, "", 0, 4, 4));
     }
 
-    public static void nqueen2() {
+    public static void nqueenPerms() {
         int n = 4, tnq = 4;
         boolean[][] box = new boolean[n][n];
-        System.out.println(nQueenPerm_01(box, tnq, ""));
+        System.out.println("NCR " + nQueenPerm_01(box, tnq, ""));
+        System.out.println("Subs " + nQueenPermuSubseq(box, tnq, "", 0, 4, 4));
+    }
+
+    public static void issafeopt() {
+        int r = 4, c = 4;
+        row = new boolean[r];
+        col = new boolean[r];
+        diag = new boolean[r + c - 1];
+        antidiag = new boolean[r + c - 1];
+        System.out.println("Is safe optimized N queen Comb " + nQueen_02(r, c, 4, "", 0));
+        System.out.println("Is safe optimized N queen Permutaions " + nQueen_02Perm(r, c, 4, "", 0));
+        System.out.println("Is safe optimized N queen First Path " + nQueen_02_firstWay(r, c, 4, "", 0));
     }
 
     public static void main(String[] args) {
-        nqueen();
-        // nqueen2();
+        // nqueenPerms();
+        // nqueenCombs();
+        issafeopt();
     }
 
 }
