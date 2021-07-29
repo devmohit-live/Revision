cc30096ccad8d7b10405620a8dd1f21edcf308ce
Mohit <mohitsingh.it5@gmail.com>
2021-07-29 09:46:37 +0530

    Subsets Using NCR | Mohit

--- Recursion/RecursionTree.java
+++ Recursion/RecursionTree.java
@@ -3,8 +3,6 @@ import java.util.*;
 public class RecursionTree {
     // ***************** */ NCR solutions
 
-
-    
     // INFINTE SUPPLY
     static int infinietePermutations(int[] coins, String asf, int tar) {
         if (tar <= 0) {
@@ -220,4 +218,52 @@ public class RecursionTree {
         return count;
     }
 
+    // INTERVIEW BIT SUBSET PROBLEM USING NCR :
+    // https://www.interviewbit.com/old/problems/subset/
+    // Actual:
+    public ArrayList<ArrayList<Integer>> subsets(ArrayList<Integer> A) {
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        ArrayList<Integer> small = new ArrayList<>();
+        Collections.sort(A);
+        subsets(A, 0, small, ans);
+
+        return ans;
+
+    }
+
+    public int subsets(ArrayList<Integer> nums, int idx, ArrayList<Integer> small, ArrayList<ArrayList<Integer>> ans) {
+        // empty subseq is counted so count=1 at start
+        // Pre Area
+        int count = 1;
+        ArrayList<Integer> base = new ArrayList<>(small);
+        ans.add(base);
+        for (int i = idx; i < nums.size(); i++) {
+            // In Arera
+            small.add(nums.get(i));
+            count += subsets(nums, i + 1, small, ans);
+            small.remove(small.size() - 1);
+        }
+        // Post area
+        return count;
+    }
+
+    // Using StringBuilder
+    public static int subsets(String str, int idx, StringBuilder asf, ArrayList<String> ans) {
+        // empty subseq is counted so count=1 at start
+        int count = 1;
+        ans.add(asf.toString());
+        for (int i = idx; i < str.length(); i++) {
+            asf.append(str.charAt(i));
+            count += subsets(str, i + 1, asf, ans);
+            asf.deleteCharAt(asf.length() - 1);
+        }
+        return count;
+    }
+
+    public static void subsets(String str) {
+        ArrayList<String> ans = new ArrayList<>();
+        StringBuilder sb = new StringBuilder("");
+        subsets(str, 0, sb, ans);
+        System.out.println(ans);
+    }
 }
