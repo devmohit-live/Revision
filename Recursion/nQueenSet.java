60e40f651388330e0f46f24e553e447108c01ece
Mohit <mohitsingh.it5@gmail.com>
2021-08-04 20:41:04 +0530

    nqueen optimised using bits | Mohit

--- Recursion/nQueenSet.java
+++ Recursion/nQueenSet.java
@@ -236,17 +236,17 @@ public class nQueenSet {
         for (int room = 0; room < n; room++) {
             int r = floor, c = room;
 
-            if ((col & (1 << c)) == 0 && (diag & (1 << (r + c))) == 0 && (adiag & (1 << (r - c + m - 1))) == 0) {
-                // section here means col,row,doag,adiag were false;
+            if ((col & (1 << c)) == 0 && (diag & (1 << (r + c))) == 0 && (adaig & (1 << (r - c + m - 1))) == 0) {
+                // section here means col,row,doag,adaig were false;
                 col ^= (1 << c);
                 diag ^= (1 << (r + c));
-                adiag ^= (1 << (r - c + m - 1));
+                adaig ^= (1 << (r - c + m - 1));
                 // col[c] = diag[r + c] = antidiag[r - c + n - 1] = true;
-                count += nqueenComb(n, tnq - 1, floor + 1, row, col, diag, adiag);
+                count += nqueenComb(n, tnq - 1, floor + 1, row, col, diag, adaig);
                 // col[c] = diag[r + c] = antidiag[r - c + n - 1] = false;
                 col ^= (1 << c);
                 diag ^= (1 << (r + c));
-                adiag ^= (1 << (r - c + m - 1));
+                adaig ^= (1 << (r - c + m - 1));
             }
         }
         return count;
