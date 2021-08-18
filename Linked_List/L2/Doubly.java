2be2bcdfbf97c7246fe353dfdd02ec7ad9a256c3
Mohit <mohitsingh.it5@gmail.com>
2021-08-19 01:58:56 +0530

    Doubly LL addAt | Mohit

--- Linked_List/L2/Doubly.java
+++ Linked_List/L2/Doubly.java
@@ -1,30 +1,64 @@
 public class Doubly {
     // dependent class
-    private class Node{
+    private class Node {
         int val;
         Node next, prev;
-        Node(int val){
-            this.val=val;
-            this.next=null;
-            this.prev=null;
+
+        Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.prev = null;
         }
     }
-    
-    //constructor
-    
 
-    private  int size;
+    // constructor
+
+    private int size;
     private Node head;
     private Node tail;
 
+    // basics functions
 
-    //basics functions
+    // ds functions
+    public void addAt(int index, int data) {
+        // here this .size bcz we can add to the sizeth index => add last
 
+        if (indexIsInvalidException(index, 0, this.size))
+            System.out.println(-1);
 
-    //ds functions
+        else {
+            Node node = new Node(data);
+            addNodeAt(index, node);
+        }
 
+    }
 
-    //exceptions
+    private void addNodeAt(int index, Node node) {
+        if (index == this.size)
+            addLastNode(node);
+        else if (index == 0)
+            addFirstNode(node);
 
+        // if(index==this.size) addLastNode(node); we have to use else if
+        // bcz for empty list idx=0 will acts ac both idx==0==this.size so we don't
+        // exacly know where to add so we will go with addFirst(can't execute both)=>
+        // exception as first addLast will ocuur then addFirst will occur on same node
+
+        else {
+
+            Node ith = getNodeAt(index);
+            Node prev = ith.prev;
+
+            node.prev = prev;
+            prev.next = node;
+
+            node.next = ith;
+            ith.prev = node;
+
+            this.size++;
+        }
+    }
+
+    // exceptions
 
 }
