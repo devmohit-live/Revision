be0d84e22ca94ad3c80fe9f5f04faa4fa4822873
Mohit <mohitsingh.it5@gmail.com>
2021-09-06 00:00:22 +0530

    Corrected Edit Distance followUp2 base condition | Mohit

--- DP/StringSet.java
+++ DP/StringSet.java
@@ -429,16 +429,24 @@ public class StringSet {
                 6, 6, 6, 7 };
         int[] delete = new int[] { 3, 4, 30, 5, 13, 6, 74, 46, 7, 10, 6, 31, 3, 8, 85, 99, 79, 20, 5, 9, 5, 10, 6, 11,
                 24, 7 };
-        int[] replace = new int[] { 9, 8, 30, 5, 13, 5, 8, 68, 5, 8, 2, 31, 83, 5, 85, 99, 79, 5, 5, 3, 5, 8, 25, 25, 6,
-                7 };
+        int[] replace = new int[] { 9, 8, 30, 5, 13, 5, 8, 68, 5, 8, 2, 31, 83, 5, 8, 5, 99, 79, 5, 5, 3, 5, 8, 25, 25,
+                6, 7 };
         String s1 = "saturday", s2 = "sunday";
         int n = s1.length(), m = s2.length();
         int[][] dp = new int[n + 1][m + 1];
         for (int[] d : dp)
             Arrays.fill(d, -1);
         int ans = minDist02(s1, s2, n, m, dp, insert, delete, replace);
-        System.out.println(ans);
+        System.out.println("Memoization:  " + ans);
         display(dp);
+
+        int[][] dp2 = new int[n + 1][m + 1];
+        for (int[] d : dp2)
+            Arrays.fill(d, -1);
+
+        int ans2 = minDist02Tab(s1, s2, n, m, dp2, insert, delete, replace);
+        System.out.println("\n\nTabulation : " + ans2);
+        display(dp2);
 
     }
 
@@ -448,22 +456,22 @@ public class StringSet {
         if (n == 0 && m == 0)
             return dp[n][m] = 0; // no operations
         if (n == 0) { // we need to add those left characters of s2
-            int sum = 0;
+            int sum = 0, itr = m;
             // making m=>0
-            while (m-- > 0) {
-                char c = s2.charAt(m);
+            while (itr-- > 0) {
+                char c = s2.charAt(itr);
                 sum += insertCost[c - 'a'];
             }
-            return sum;
+            return dp[n][m] = sum;
         }
         if (m == 0) { // we need to delete those extra characters from s1
-            int sum = 0;
+            int sum = 0, itr = n;
             // making n=>0
-            while (n-- > 0) {
-                char c = s1.charAt(n);
+            while (itr-- > 0) {
+                char c = s1.charAt(itr);
                 sum += deleteCost[c - 'a'];
             }
-            return sum;
+            return dp[n][m] = sum;
         }
 
         char a = s1.charAt(n - 1), b = s2.charAt(m - 1);
@@ -475,7 +483,7 @@ public class StringSet {
         delete += deleteCost[a - 'a']; // must have deleted corresponding char of s1
 
         int replace = minDist02(s1, s2, n - 1, m - 1, dp, insertCost, deleteCost, replaceCost);
-        replace += replaceCost[a - 'a']; // or b any onw will work
+        replace += replaceCost[a - 'a']; // replaced ch1 from s1
 
         int ans = 0;
 
@@ -487,6 +495,62 @@ public class StringSet {
             ans = Math.min(Math.min(insert, delete), replace);
         }
         return dp[n][m] = ans;
+    }
+
+    static int minDist02Tab(String s1, String s2, int N, int M, int[][] dp, int[] insertCost, int[] deleteCost,
+            int[] replaceCost) {
+        for (int n = 0; n <= N; n++) {
+            for (int m = 0; m <= M; m++) {
+                if (n == 0 && m == 0) {
+                    dp[n][m] = 0; // no operations
+                    continue;
+                }
+
+                if (n == 0) { // we need to add those left characters of s2
+                    int sum = 0, itr = m;
+                    // making m=>0
+                    while (itr-- > 0) {
+                        char c = s2.charAt(itr);
+                        sum += insertCost[c - 'a'];
+                    }
+                    dp[n][m] = sum;
+                    continue;
+                }
+                if (m == 0) { // we need to delete those extra characters from s1
+                    int sum = 0, itr = n;
+                    // making n=>0
+                    while (itr-- > 0) {
+                        char c = s1.charAt(itr);
+                        sum += deleteCost[c - 'a'];
+                    }
+                    dp[n][m] = sum;
+                    continue;
+                }
+
+                char a = s1.charAt(n - 1), b = s2.charAt(m - 1);
+
+                int insert = minDist02(s1, s2, n, m - 1, dp, insertCost, deleteCost, replaceCost);
+                insert += +insertCost[b - 'a']; // must have inserted corresponding char of s2 into a s1
+
+                int delete = minDist02(s1, s2, n - 1, m, dp, insertCost, deleteCost, replaceCost);
+                delete += deleteCost[a - 'a']; // must have deleted corresponding char of s1
+
+                int replace = minDist02(s1, s2, n - 1, m - 1, dp, insertCost, deleteCost, replaceCost);
+                replace += replaceCost[a - 'a']; // or b any onw will work
+
+                int ans = 0;
+
+                if (a == b) {
+                    // no cost
+                    ans = minDist02(s1, s2, n - 1, m - 1, dp, insertCost, deleteCost, replaceCost);
+                } else {
+                    // some operation is needed
+                    ans = Math.min(Math.min(insert, delete), replace);
+                }
+                dp[n][m] = ans;
+            }
+        }
+        return dp[N][M];
     }
 
     // Dependecy of : 48, 10
@@ -509,7 +573,7 @@ public class StringSet {
     }
 
     // Leetocde 44 : WildCard
-
+    // -1 -> default, 0 -> false, 1 -> true
     public boolean isMatch(String s, String p) {
         String newp = removeDuplicateStars(p);
         System.out.println(newp);
