2fed40b643d9900d26593df297269c8d5ed7ad69
Mohit <mohitsingh.it5@gmail.com>
2021-09-05 20:11:02 +0530

    Edit Distance : Each character have its' own cost for specific operation | Mohit

--- DP/StringSet.java
+++ DP/StringSet.java
@@ -422,6 +422,73 @@ public class StringSet {
         return dp[N][M];
     }
 
+    // Min Ditance FollowUP2 : each character have individual cost for different
+    // operation
+    static void minDistanceFollowUp2_run() {
+        int[] insert = new int[] { 43, 96, 30, 37, 13, 19, 74, 68, 89, 10, 76, 31, 83, 45, 85, 99, 79, 20, 5, 3, 5, 1,
+                6, 6, 6, 7 };
+        int[] delete = new int[] { 3, 4, 30, 5, 13, 6, 74, 46, 7, 10, 6, 31, 3, 8, 85, 99, 79, 20, 5, 9, 5, 10, 6, 11,
+                24, 7 };
+        int[] replace = new int[] { 9, 8, 30, 5, 13, 5, 8, 68, 5, 8, 2, 31, 83, 5, 85, 99, 79, 5, 5, 3, 5, 8, 25, 25, 6,
+                7 };
+        String s1 = "saturday", s2 = "sunday";
+        int n = s1.length(), m = s2.length();
+        int[][] dp = new int[n + 1][m + 1];
+        for (int[] d : dp)
+            Arrays.fill(d, -1);
+        int ans = minDist02(s1, s2, n, m, dp, insert, delete, replace);
+        System.out.println(ans);
+        display(dp);
+
+    }
+
+    static int minDist02(String s1, String s2, int n, int m, int[][] dp, int[] insertCost, int[] deleteCost,
+            int[] replaceCost) {
+
+        if (n == 0 && m == 0)
+            return dp[n][m] = 0; // no operations
+        if (n == 0) { // we need to add those left characters of s2
+            int sum = 0;
+            // making m=>0
+            while (m-- > 0) {
+                char c = s2.charAt(m);
+                sum += insertCost[c - 'a'];
+            }
+            return sum;
+        }
+        if (m == 0) { // we need to delete those extra characters from s1
+            int sum = 0;
+            // making n=>0
+            while (n-- > 0) {
+                char c = s1.charAt(n);
+                sum += deleteCost[c - 'a'];
+            }
+            return sum;
+        }
+
+        char a = s1.charAt(n - 1), b = s2.charAt(m - 1);
+
+        int insert = minDist02(s1, s2, n, m - 1, dp, insertCost, deleteCost, replaceCost);
+        insert += +insertCost[b - 'a']; // must have inserted corresponding char of s2 into a s1
+
+        int delete = minDist02(s1, s2, n - 1, m, dp, insertCost, deleteCost, replaceCost);
+        delete += deleteCost[a - 'a']; // must have deleted corresponding char of s1
+
+        int replace = minDist02(s1, s2, n - 1, m - 1, dp, insertCost, deleteCost, replaceCost);
+        replace += replaceCost[a - 'a']; // or b any onw will work
+
+        int ans = 0;
+
+        if (a == b) {
+            // no cost
+            ans = minDist02(s1, s2, n - 1, m - 1, dp, insertCost, deleteCost, replaceCost);
+        } else {
+            // some operation is needed
+            ans = Math.min(Math.min(insert, delete), replace);
+        }
+        return dp[n][m] = ans;
+    }
+
     // Dependecy of : 48, 10
     String removeDuplicateStars(String s) {
         if (s.length() == 0)
@@ -535,13 +602,15 @@ public class StringSet {
     // Leetcode 10
     public static void main(String[] args) {
         // String s1 = "geek", s2 = "acef";
-        String s = "geeksforgeeks", t = "gks";
-        // lcs(s1, s2);
-        int n = s.length(), m = t.length();
-        int[][] dp = new int[n + 1][m + 1];
-        for (int[] d : dp)
-            Arrays.fill(d, -1);
-        System.out.println(mindistinctSubsTab(s, t, n, m, dp));
-        display(dp);
+        // String s = "geeksforgeeks", t = "gks";
+        // // lcs(s1, s2);
+        // int n = s.length(), m = t.length();
+        // int[][] dp = new int[n + 1][m + 1];
+        // for (int[] d : dp)
+        // Arrays.fill(d, -1);
+        // System.out.println(mindistinctSubsTab(s, t, n, m, dp));
+        // display(dp);
+
+        minDistanceFollowUp2_run();
     }
 }
