f8900cecdeb3637cf2b9adacb02c544246584fdb
Mohit <mohitsingh.it5@gmail.com>
2021-09-16 00:56:39 +0530

    Russian Doll Envelopes | Mohit

--- DP/lis.java
+++ DP/lis.java
@@ -362,6 +362,32 @@ public class lis {
         return max;
     }
 
+    // Leetcode 354 : Russian Doll Envelope
+    public int maxEnvelopes(int[][] envelopes) {
+        // sort them on basis of height
+        Arrays.sort(envelopes, (a, b) -> {
+            return a[1] - b[1];
+        });
+
+        // do lis on basis og widht (strictly greater)
+        // to avoid duplicates in sorted height use a if case too
+
+        int n = envelopes.length, max = 0;
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for (int j = i - 1; j >= 0; j--) {
+                if (envelopes[j][0] < envelopes[i][0] && envelopes[i][1] > envelopes[j][1]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+
+            }
+
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+
     public static void main(String[] args) {
 
         // System.out.println(longestIncreasingSubsequence());
