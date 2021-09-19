2726ffa8d27afc704ba5e59a2b1d4a1e1a9dbc81
Mohit <mohitsingh.it5@gmail.com>
2021-09-19 22:16:59 +0530

    MCM with Path | Mohit

---
+++ DP/MCM_CutProblems.java
@@ -0,0 +1,126 @@
+import java.util.Arrays;
+
+public class MCM_CutProblems {
+    public static void display2d(int[][] dp) {
+        for (int[] d : dp)
+            System.out.println(Arrays.toString(d));
+    }
+
+    static int matrixMultiplication(int N, int arr[]) {
+        int[][] dp = new int[N][N];
+        for (int[] d : dp)
+            Arrays.fill(d, -1);
+
+        int ans = mcm_Mem(arr, 0, N - 1, dp);
+        display2d(dp);
+        int[][] dp2 = new int[N][N];
+        int ans2 = mcm_Tab(arr, 0, N - 1, dp2);
+        return ans;
+    }
+
+    static int mcm_Mem(int arr[], int si, int ei, int[][] dp) {
+
+        // single matrix
+        if (ei - si == 1) {
+            return dp[si][ei] = 0;
+        }
+
+        if (dp[si][ei] != -1)
+            return dp[si][ei];
+
+        // make valid cuts
+        int ans = (int) 1e9;
+        for (int cut = si + 1; cut < ei; cut++) {
+            int leftSideMultiplication = mcm_Mem(arr, si, cut, dp);
+            int rightSideMultiplication = mcm_Mem(arr, cut, ei, dp);
+            int leftAndRighMultiplication = arr[si] * arr[cut] * arr[ei];
+            int net = leftSideMultiplication + rightSideMultiplication + leftAndRighMultiplication;
+            ans = Math.min(ans, net);
+        }
+
+        return dp[si][ei] = ans;
+
+    }
+
+    static int mcm_Tab(int[] arr, int SI, int EI, int[][] dp) {
+        // gap=0 represents invalid 2d matrix => 1d
+        int n = arr.length;
+        for (int gap = 1; gap < n; gap++) {
+            for (int si = SI, ei = gap; si < n && ei <= EI; si++, ei++) {
+                if (ei - si == 1) {
+                    dp[si][ei] = 0;
+                    continue;
+                }
+
+                // make valid cuts
+                int ans = (int) 1e9;
+                for (int cut = si + 1; cut < ei; cut++) {
+                    int leftSideMultiplication = dp[si][cut]; // mcm_Mem(arr, si, cut, dp);
+                    int rightSideMultiplication = dp[cut][ei];// mcm_Mem(arr, cut, ei, dp);
+                    int leftAndRighMultiplication = arr[si] * arr[cut] * arr[ei];
+                    int net = leftSideMultiplication + rightSideMultiplication + leftAndRighMultiplication;
+                    ans = Math.min(ans, net);
+                }
+
+                dp[si][ei] = ans;
+
+            }
+        }
+        System.out.println("DP of Tabulation: ");
+        display2d(dp);
+        return dp[SI][EI];
+    }
+
+    // GFG:
+    // https://practice.geeksforgeeks.org/problems/brackets-in-matrix-chain-multiplication1024/1#
+    static String matrixChainOrder(int p[], int n) {
+        int[][] dp = new int[n][n];
+        String[][] sdp = new String[n][n];
+        String ans = mcm_Tab_with_Path(p, 0, n - 1, dp, sdp);
+        return ans;
+    }
+
+    static String mcm_Tab_with_Path(int[] arr, int SI, int EI, int[][] dp, String[][] sdp) {
+        // gap=0 represents invalid 2d matrix => 1d
+        int n = arr.length;
+        for (int gap = 1; gap < n; gap++) {
+            for (int si = SI, ei = gap; si < n && ei <= EI; si++, ei++) {
+                if (ei - si == 1) {
+                    dp[si][ei] = 0;
+                    sdp[si][ei] = (char) (si + 'A') + "";
+                    continue;
+                }
+
+                // make valid cuts
+                int ans = (int) 1e9;
+                String sans = "";
+
+                for (int cut = si + 1; cut < ei; cut++) {
+                    int leftSideMultiplication = dp[si][cut]; // mcm_Mem(arr, si, cut, dp);
+                    int rightSideMultiplication = dp[cut][ei];// mcm_Mem(arr, cut, ei, dp);
+                    int leftAndRighMultiplication = arr[si] * arr[cut] * arr[ei];
+                    int net = leftSideMultiplication + rightSideMultiplication + leftAndRighMultiplication;
+                    if (ans > net) {
+                        ans = net;
+                        sans = "(" + sdp[si][cut] + sdp[cut][ei] + ")";
+                    }
+
+                }
+
+                dp[si][ei] = ans;
+                sdp[si][ei] = sans;
+
+            }
+        }
+        // System.out.println("DP of Tabulation: ");
+        // display2d(dp);
+        return sdp[SI][EI];
+    }
+
+    public static void main(String[] args) {
+        int N = 5;
+        int[] arr = { 40, 20, 30, 10, 30 };
+        // Output: 26000
+        matrixMultiplication(N, arr);
+    }
+}
--- DP/Questions.java
+++ DP/Questions.java
@@ -292,6 +292,9 @@ public class Questions {
         return dp[idx] = count;
     }
 
+    
+    
+    
     // GFG MaxGold:
 
     static int maxGold(int n, int m, int M[][]) {
@@ -487,6 +490,50 @@ public class Questions {
         }
         return b;
     }
+
+ // Leetcode 139 : WordBreak
+    
+    public boolean wordBreak(String s, List<String> wordDict) {
+        HashSet<String> set = new HashSet<>();
+        int maxwordlen = 0, n = s.length();
+        boolean[] dp = new boolean[n + 1]; // is word till i possible
+
+        for (String ss : wordDict) {
+            maxwordlen = Math.max(ss.length(), maxwordlen);
+            set.add(ss);
+        }
+
+        return wordBreak(s, set, dp, n, maxwordlen);
+
+    }
+
+    boolean wordBreak(String s, HashSet<String> set, boolean[] dp, int n, int maxlen) {
+
+        dp[0] = true; // "" string
+
+        for (int i = 0; i <= n; i++) {
+            // move forward if we found false;
+            if (!dp[i])
+                continue;
+
+            // for true we have to check different possibilites of making a word seperation
+            // : finding the end point index upto which word can be seperated
+
+            // check for the words=>length=[1,maxlen]
+            for (int l = 1; l <= maxlen && l + i <= n; l++) {
+                String substring = s.substring(i, l + i);
+                if (set.contains(substring))
+                    dp[l + i] = true;
+            }
+
+        }
+        // System.out.println(set); System.out.println(Arrays.toString(dp));
+
+        return dp[n];
+    }
+
+
+
 
     // We have done this as we just need no of ways , so we used the concept that n
     // similar calls will give same result so net = n*x
