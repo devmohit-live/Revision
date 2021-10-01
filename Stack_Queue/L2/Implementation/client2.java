fdc8867b009a99a608370941cfb5e0a7a8ca2dd2
Mohit <mohitsingh.it5@gmail.com>
2021-10-01 23:36:05 +0530

    Implemeted Queue | Mohit

--- Stack_Queue/L2/Implementation/client2.java
+++ Stack_Queue/L2/Implementation/client2.java
@@ -1,6 +1,21 @@
 public class client2 {
     public static void main(String[] args) throws Exception {
-        Q q = new Q(5);
+        // Q q = new Q(5);
+        // System.out.println(q.getSize());
+        // q.add(1);
+        // q.add(2);
+        // q.add(3);
+        // q.add(4);
+        // q.add(5);
+        // System.out.println(q);
+        // System.out.println(q.getSize());
+        // System.out.println(q.remove());
+        // System.out.println(q.remove());
+        // System.out.println(q);
+        // System.out.printf("Size of Q is : %d , Capacity is : %d \n", q.getSize(),
+        // q.getMaxCapacity());
+
+        DynamicQ q = new DynamicQ(5);
         System.out.println(q.getSize());
         q.add(1);
         q.add(2);
@@ -9,10 +24,14 @@ public class client2 {
         q.add(5);
         System.out.println(q);
         System.out.println(q.getSize());
-        System.out.println(q.remove());
-        System.out.println(q.remove());
+        q.add(100);
         System.out.println(q);
         System.out.printf("Size of Q is : %d , Capacity is : %d \n", q.getSize(), q.getMaxCapacity());
+        // System.out.println(q.remove());
+        // System.out.println(q.remove());
+        // System.out.println(q);
+        // System.out.printf("Size of Q is : %d , Capacity is : %d \n", q.getSize(),
+        // q.getMaxCapacity());
 
     }
 }
