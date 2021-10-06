bfb8510f74f929c078fefca75aa8ad1ce0394b62
Mohit <mohitsingh.it5@gmail.com>
2021-10-07 00:32:00 +0530

    Q Using Stack Amortized O(1) | Mohit

--- Stack_Queue/L2/Implementation/Adapters/QUisngStack.java
+++ Stack_Queue/L2/Implementation/Adapters/QUisngStack.java
@@ -107,4 +107,71 @@ public class QUisngStack {
         }
     }
 
+    // follow Up : Make the Stack AMortized O(1):
+
+    // Basically we dont swap back the temp stack to original stack
+    // at every pop /top opeartion which was taking O(2n) time for every single
+    // opeartion, we first check whether the tmp stack
+    // has something, if it has then give that directly if not swap to get the items
+    // in stack
+
+    // Since in Q fifo is followed so while we pop at any stage the previosly added
+    // elements will be there in tmp stack
+    // even if newly added elements are in st stack, for every pop(remove in q)
+    // elemets from tmp st will be deleted first
+
+    class FU {
+        private Stack<Integer> st;
+        private Stack<Integer> tmp;
+        int size;
+
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        this.st =new Stack<>();
+        this.tmp =new Stack<>();
+        this.size =0;
+    }
+
+        /** Push element x to the back of queue. */
+        public void push(int x) {
+            this.size++;
+            this.st.push(x);
+        }
+
+        private void swap(Stack<Integer> a, Stack<Integer> b) {
+            while (a.size() != 0) {
+                b.push(a.pop());
+
+            }
+
+        }
+
+        /**
+         * Check if the op/tmp stack has some element(in reverse/actual q order) if not
+         * swap the stack , and get the element
+         */
+        public int pop() {
+            if (tmp.size() == 0)
+                swap(st, tmp);
+
+            int data = tmp.pop();
+            this.size--;
+            return data;
+        }
+
+        /** Get the front element. */
+        public int peek() {
+            if (tmp.size() == 0)
+                swap(st, tmp);
+
+            int data = tmp.peek();
+            return data;
+        }
+
+        /** Returns whether the queue is empty. */
+        public boolean empty() {
+            return this.size == 0;
+        }
+    }
+
 }
