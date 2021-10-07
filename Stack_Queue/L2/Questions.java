29e98f4b92a3bae3519b690162373613d9317470
Mohit <mohitsingh.it5@gmail.com>
2021-10-07 23:41:29 +0530

    Longest Valid Paranthesis Seq | Mohit

--- Stack_Queue/L2/Questions.java
+++ Stack_Queue/L2/Questions.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 public class Questions {
     // Leetcode 20 : Valid Parantheis
     public boolean isValid(String s) {
@@ -167,4 +169,111 @@ public class Questions {
         return st.pop();
     }
 
+    // Leetcode 84 : Largest Area of Histogram
+
+    // Using both boundaries:
+    public int largestRectangleArea(int[] heights) {
+        // next smaller on both sides(left,right) => will give excluding boundaries
+        // height is already given , area = height * (B - A) -1 (b= nsor , a = nsol)(as
+        // both boundaries are excluded)
+
+        int[] nsol = nsol(heights);
+        int[] nsor = nsor(heights);
+        // System.out.println("Left boundary "+ Arrays.toString(nsol));
+        // System.out.println("Right boundary "+ Arrays.toString(nsor));
+
+        int area = 0, n = heights.length;
+
+        for (int i = 0; i < n; i++) {
+            int ht = heights[i];
+            int rightExcludedBoundary = nsor[i];
+            int leftExcludedBoundary = nsol[i];
+
+            int width = rightExcludedBoundary - leftExcludedBoundary - 1;
+            int currarea = ht * width;
+            // System.out.println(i+" "+width+" "+currarea);
+            area = Math.max(area, currarea);
+        }
+
+        return area;
+    }
+
+    // Just calculating the right boundary and procssing left in place with right
+    // O(2n)
+    public int largestRectangleArea2(int[] heights) {
+        int area = 0, n = heights.length;
+        Stack<Integer> st = new Stack<>();
+        st.push(-1); // ref for end of st
+
+        // nsor kind of thing
+        for (int i = 0; i < n; i++) {
+            while (st.peek() != -1 && heights[st.peek()] > heights[i]) {
+                int ht = heights[st.pop()];
+                int width = i - st.peek() - 1; // right boundary = i , left = st.peek()
+                area = Math.max(area, ht * width);
+            }
+
+            st.push(i);
+        }
+
+        // some elemtns left in stack => for them right boundary is arr.length = n;
+        while (st.peek() != -1) {
+            int ht = heights[st.pop()];
+            int width = n - st.peek() - 1; // right boundary = n, left = st.peek()
+            area = Math.max(area, ht * width);
+        }
+
+        return area;
+    }
+
+    // Leetcode 85 : Maximal Rectangle: Treat every row as histogram's base
+
+    // O(3nm)
+    public int maximalRectangle(char[][] matrix) {
+        if (matrix.length == 0 || matrix[0].length == 0)
+            return 0;
+
+        int n = matrix.length, m = matrix[0].length, area = 0, curr = 0;
+
+        int[] height = new int[m]; // prefix arr representing histogram at a time
+
+        for (int i = 0; i < n; i++) {// O(n)
+            for (int j = 0; j < m; j++) { // O(m)
+                // if matrix[i][j] was zero at base then no histogram is possible else
+                // matrix[][] = 1 add h=1 more to the heihgt
+                height[j] = matrix[i][j] == '0' ? 0 : height[j] + 1;
+            }
+            curr = largestRectangleArea2(height);// O(2m)
+            area = Math.max(area, curr);
+        }
+        return area;
+    }
+
+    // Leetcode 32: Longest Valid Parentheses
+    // based on lasrgest area histogram 2
+
+    public int longestValidParentheses(String s) {
+        int ans = 0, n = s.length();
+        if (n == 0)
+            return 0;
+
+        Stack<Integer> st = new Stack<>();
+        st.push(-1);
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if (ch == ')' && st.peek() != -1 && s.charAt(st.peek()) == '(') {
+                st.pop();
+                ans = Math.max(ans, i - st.peek()); // length
+            }
+
+            else
+                st.push(i); // either '(' or ')'
+
+        }
+
+        // for longest invalid seq return st.size() -1 // the leftovers in stack -1(bcz
+        // we have manually added extra -1 at start)
+
+        return ans;
+    }
 }
