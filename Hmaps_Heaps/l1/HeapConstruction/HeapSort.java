b71b921a9a1d3a058ef5db96d6ede23ef6bddcb3
Mohit <mohitsingh.it5@gmail.com>
2021-10-15 23:51:30 +0530

    Trap rain water

--- Hmaps_Heaps/l1/HeapConstruction/HeapSort.java
+++ Hmaps_Heaps/l1/HeapConstruction/HeapSort.java
@@ -62,3 +62,4 @@ public class HeapSort {
         // heapSort(arr, false);
     }
 }
+
--- Stack_Queue/L2/Questions.java
+++ Stack_Queue/L2/Questions.java
@@ -679,4 +679,28 @@ public class Questions {
         return ans;
     }
 
+    // trapping rain water using largest area histogramm concept:
+    // https://leetcode.com/problems/trapping-rain-water/discuss/17414/A-stack-based-solution-for-reference-inspired-by-Histogram
+    public int trap(int[] height) {
+        if (height == null || height.length < 2)
+            return 0;
+
+        Stack<Integer> stack = new Stack<>();
+        int water = 0, i = 0;
+        while (i < height.length) {
+            if (stack.isEmpty() || height[i] <= height[stack.peek()]) {
+                stack.push(i++);
+            } else {
+                int pre = stack.pop();
+                if (!stack.isEmpty()) {
+                    // find the smaller height between the two sides
+                    int minHeight = Math.min(height[stack.peek()], height[i]);
+                    // calculate the area
+                    water += (minHeight - height[pre]) * (i - stack.peek() - 1);
+                }
+            }
+        }
+        return water;
+    }
+
 }
