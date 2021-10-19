fcfb0df0d3de35843e3705ec796da3a0577cf7f1
Mohit <mohitsingh.it5@gmail.com>
2021-10-19 22:28:44 +0530

    Longest Consecutive Subsequence | Mohit

--- Hmaps_Heaps/l2/heaps/Questions.java
+++ Hmaps_Heaps/l2/heaps/Questions.java
@@ -260,4 +260,37 @@ public class Questions {
         return n - 1;
 
     }
+
+    // Leetcode 128 : Longest Consecutive Subsequnce
+    public int longestConsecutive(int[] nums) {
+
+        // idea is to remove the consecutive elemets from the set as we explre them
+        // once, so that all the consecutive elemets related to an element e is removed
+        // at once
+        if (nums.length == 0 || nums.length == 1)
+            return nums.length;
+        int maxlen = 0, start = -1;
+        Set<Integer> set = new HashSet<>();
+        for (int el : nums)
+            set.add(el);
+        int max = 0;
+        for (int el : nums) {
+            int prev = el - 1, next = el + 1;
+
+            // set.remove actually decreases the search complexity/time of element in a set
+            while (set.contains(prev))
+                set.remove(prev--);
+            while (set.contains(next))
+                set.remove(next++);
+            int len = next - prev - 1;
+            if (len > maxlen) {
+                maxlen = len;
+                start = prev + 1;
+            }
+        }
+
+        // System.out.println(start+" "+(start+maxlen-1));
+        return maxlen;
+
+    }
 }
