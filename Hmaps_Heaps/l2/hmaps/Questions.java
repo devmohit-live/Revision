7609bf243d829fd9e98ad4baa5a613d6aace4a73
Mohit <mohitsingh.it5@gmail.com>
2021-10-20 20:32:17 +0530

    1424 | Mohit

--- Hmaps_Heaps/l2/hmaps/Questions.java
+++ Hmaps_Heaps/l2/hmaps/Questions.java
@@ -32,25 +32,62 @@ public class Questions {
 
     }
 
-//Leetcode 1218 : . Longest Arithmetic Subsequence of Given Difference
+    // Leetcode 1218 : . Longest Arithmetic Subsequence of Given Difference
 
-     public int longestSubsequence(int[] nums, int diff) {
-        int max = -1,n=nums.length;
+    public int longestSubsequence(int[] nums, int diff) {
+        int max = -1, n = nums.length;
         // since here we aleady know the difference is cinstant ,
         // we eill be kwwp on checking for the previos element exists in map beforehand
-        
-        // why not recursion of lis bcz here we already know diff, it is not a normal subseq, it is an arthmetic subseq
-        
-        //el, length of lis upto now
-        HashMap<Integer,Integer> map = new HashMap<>();
-        
-        for(int el: nums){
-            map.put(el, map.getOrDefault(el-diff,0)+1);
+
+        // why not recursion of lis bcz here we already know diff, it is not a normal
+        // subseq, it is an arthmetic subseq
+
+        // el, length of lis upto now
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int el : nums) {
+            map.put(el, map.getOrDefault(el - diff, 0) + 1);
         }
-        
-        for(int el: map.keySet())
-            max =Math.max(max,map.get(el));
-        
+
+        for (int el : map.keySet())
+            max = Math.max(max, map.get(el));
+
         return max;
+    }
+
+    // Leetcode : 1424. Diagonal Traverse II
+    public int[] findDiagonalOrder(List<List<Integer>> nums) {
+        // diag no(i+j) (left to right), corresponding elements
+        HashMap<Integer, LinkedList<Integer>> map = new HashMap<>();
+        int size = 0, n = nums.size(), max = 0, idx = 0;
+
+        // addFirst bcz we want the order such that => leftbottom -> upperright ie we
+        // want to (3,1) before (1,3)
+        // but we will be getting 1,3 first
+
+        // (changint the loop in reverse order causes problem too)
+
+        for (int i = 0; i < n; i++) {
+            size += nums.get(i).size();
+            for (int j = 0; j < nums.get(i).size(); j++) {
+                int diag = i + j;
+                int el = nums.get(i).get(j);
+                map.putIfAbsent(diag, new LinkedList<Integer>());
+                map.get(diag).addFirst(el); // added to corresponding diag list into correct order
+                // get the maximum diag value so that we can traverse from 0->max(in order to
+                // fill in the answer)
+                max = Math.max(max, diag);
+            }
+        }
+
+        int[] ans = new int[size];
+        for (int i = 0; i <= max; i++) {
+            LinkedList<Integer> tmp = map.get(i);
+            while (tmp.size() != 0)
+                ans[idx++] = tmp.removeFirst();
+
+        }
+
+        return ans;
     }
 }
