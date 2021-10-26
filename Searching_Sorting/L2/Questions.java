7e895a12db5272b70ac6114945cb614f9e139daa
Mohit <mohitsingh.it5@gmail.com>
2021-10-26 22:10:22 +0530

    Better Approach | Mohit

--- Searching_Sorting/L2/Questions.java
+++ Searching_Sorting/L2/Questions.java
@@ -55,4 +55,121 @@ public class Questions {
         return -1;
     }
 
+    // closest to the target (if present then return target) else return closes to
+    // it (left oriented)
+    int closestPoint(int[] arr, int tar) {
+        int si = 0, ei = arr.length - 1, diff = (int) 1e9, ans = -1;
+        while (si <= ei) {
+            int mid = si + (ei - si) / 2;
+            if (arr[mid] == tar)
+                return tar;
+
+            if (diff < Math.abs(tar - arr[mid])) {
+                diff = Math.abs(tar - arr[mid]);
+                ans = arr[mid];
+            }
+
+            else if (arr[mid] > tar) {
+                ei = mid - 1;
+            } else {
+                si = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    // returns index
+    // or : si,ei will be actual ceil and floor in the end at last si crosses ei and
+    // beomes ceil, ei becoomes floor
+    int closestPointBetter(int[] arr, int tar) {
+        int si = 0, ei = arr.length - 1;
+
+        // corner
+        if (data < arr[si])
+            return si;
+        else if (data > arr[ei])
+            return ei;
+
+        while (si <= ei) {
+            int mid = (si + ei) / 2;
+            if (arr[mid] == data)
+                return mid;
+            else if (arr[mid] < data)
+                si = mid + 1;
+            else
+                ei = mid - 1;
+        }
+
+        return data - arr[ei] < arr[si] - data ? ei : si;
+    }
+
+    // perfect location :
+
+    int perfectLocation(int[] arr, int tar) {
+        if (arr[0] > tar)
+            return 0;
+        int n = arr.length;
+        if (arr[n - 1] < tar)
+            return n;
+        else {
+            int si = 0, ei = n - 1;
+            while (si <= ei) {
+                int mid = si + (ei - si) / 2;
+                if (arr[mid] > tar) {
+                    ei = mid - 1;
+                } else if (arr[mid] < tar) {
+                    si = mid + 1;
+                } else {
+
+                    if (mid + 1 <= ei && arr[mid + 1] > tar)
+                        si = mid + 1;
+                    else
+                        return mid + 1;
+
+                }
+            }
+        }
+        return -1;
+    }
+
+    // or perfect location: ei will be pointing to the potential index
+    int perfectLocationBetter(int[] arr, in tar){
+        int si =0, ei = arr.length; // not -1 as element can be added to last 
+        // as here we don't want si to cross ei(potential index)
+        while(si<ei){
+            int mid = si + (ei - si )/2;
+            if(arr[mid]<=tar) si = mid+1; //damn sure about next idx as me or previos are lesser
+            else ei = mid, // not mid-1 as it can be the case that from 0 to mid-1 be the same duplicate element == tar
+
+        }
+
+        return ei;
+    }
+
+    // ceil and floor
+    int[] ceilAndfloor(int[] arr, int tar) {
+        int si = 0, ei = arr.length - 1;
+
+        // corner
+        if (data < arr[si])
+            return si;
+        else if (data > arr[ei])
+            return ei;
+
+        while (si <= ei) {
+            int mid = (si + ei) / 2;
+            if (arr[mid] == data)
+                return new int[] { mid, mid };
+            // ..or if truely wanted the floor ceil
+            // return new int[]{(mid-1),{mid+1}};
+            // it can be -1, n
+            else if (arr[mid] < data)
+                si = mid + 1;
+            else
+                ei = mid - 1;
+        }
+
+        return new int[] { ei, si }; // ceil,floor(si crosses over ei)
+    }
+
 }
