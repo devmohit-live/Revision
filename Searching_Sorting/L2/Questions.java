b759ea285e614a7365595d7c43b587df5c44d8a9
Mohit <mohitsingh.it5@gmail.com>
2021-10-26 20:05:23 +0530

    First and Last Index | Mohit

--- Searching_Sorting/L2/Questions.java
+++ Searching_Sorting/L2/Questions.java
@@ -1,3 +1,58 @@
 public class Questions {
-    
+    // Leetcode 34
+
+    public int[] searchRange(int[] num, int target) {
+        return new int[] { firstIndex(num, target), lastIndex(num, target) };
+    }
+
+    private static int firstIndex(int[] arr, int data) {
+        int si = 0, ei = arr.length - 1;
+
+        while (si <= ei) {
+            int mid = si + (ei - si) / 2;
+
+            if (data < arr[mid])
+                ei = mid - 1;
+            else if (data > arr[mid])
+                si = mid + 1;
+            else {
+                // equal
+
+                // more elements are present to left
+                if (mid - 1 >= 0 && arr[mid - 1] == data)
+                    ei = mid - 1;
+
+                else
+                    return mid;
+            }
+        }
+
+        return -1;
+    }
+
+    private static int lastIndex(int[] arr, int data) {
+        int si = 0, ei = arr.length - 1;
+
+        while (si <= ei) {
+            int mid = si + (ei - si) / 2;
+
+            if (data < arr[mid])
+                ei = mid - 1;
+            else if (data > arr[mid])
+                si = mid + 1;
+            else {
+                // equal
+
+                // more elements are present to right
+                if (mid + 1 < arr.length && arr[mid + 1] == data)
+                    si = mid + 1;
+
+                else
+                    return mid;
+            }
+        }
+
+        return -1;
+    }
+
 }
