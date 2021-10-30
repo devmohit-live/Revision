c41c63b078227f5739b6274a920e006ea66125b7
Mohit <mohitsingh.it5@gmail.com>
2021-10-30 23:34:57 +0530

    4sumcount | Mohit

--- Arrays And String/Questions.java
+++ Arrays And String/Questions.java
@@ -110,6 +110,98 @@ public class Questions {
         return res;
     }
 
+    // Leetcode 167 2sum 2
+    public int[] twoSum2(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            int f = target - nums[i];
+            if (map.containsKey(f)) {
+                return new int[] { map.get(f), i + 1 };
+            } else {
+                map.put(nums[i], i + 1);
+            }
+        }
+
+        return new int[] { -1, -1 };
+    }
+
     // KSUM
+    public void makeAns(List<List<Integer>> ans, List<List<Integer>> smallAns, int fix) {
+        for (List<Integer> s : smallAns) {
+            s.add(fix);
+            ans.add(s);
+        }
+    }
+
+    public void ksum(int[] arr, int k, int target) {
+        KSum(arr, target, k, 0, nums.length - 1);
+    }
+
+    public List<List<Integer>> KSum(int[] nums, int target, int k, int si, int ei) {
+        if (k == 2) {
+            return 2sum(nums, target, si, ei);
+        }
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for (int i = si; i < ei; i++) {
+            int fix = nums[i];
+            List<Integer> smallAns = KSum(nums, target - fix, k - 1, i + 1, ei);
+            makeAns(ans, smallAns, fix);
+            i++;
+
+            // to remove duplicates
+            while (i < ei && nums[i] == nums[i - 1])
+                i++;
+        }
+
+        return ans;
+    }
+
+    // https:// practice.geeksforgeeks.org/problems/count-pair-sum5956/1# with
+    // duplicates allowed
+
+    // here the trick was to so ans + = map.get(tar - num[i]) as duplicate items
+    // forms multiple answers, that why map not set
+    // as freq needs to be stored
+
+    int countPairs(int arr1[], int arr2[], int M, int N, int x) {
+        int ans = 0;
+        HashMap<Integer, Integer> set = new HashMap<>();
+        for (int el : arr2)
+            set.put(el, set.getOrDefault(el, 0) + 1);
+        for (int i = 0; i < M; i++)
+            if (set.containsKey(x - arr1[i]))
+                ans += set.get(x - arr1[i]);
+
+        return ans;
+    }
+
+    // Leetcode 454
+    // here th etrick was to combine 2 arrays into one find in O(1) for compliment
+    // any combination is possible so maintainf frequncey is required
+    // getting th ecompliment n time results in n answers
+
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int target = 0;
+        for (int e : nums1) {
+            for (int f : nums2) {
+                map.put(e + f, map.getOrDefault(e + f, 0) + 1);
+            }
+        }
+
+        int ans = 0;
+        for (int e : nums3) {
+            for (int f : nums4) {
+                if (map.containsKey(target - (e + f))) {
+                    ans += map.get(target - (e + f));
+                }
+            }
+        }
+
+        return ans;
+    }
 
 }
