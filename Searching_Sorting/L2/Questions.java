7ba49efc84637f8f23903708abe37bdd79bd313b
Mohit <mohitsingh.it5@gmail.com>
2021-11-03 22:52:15 +0530

    153 Min in sorted rotated array | Mohit

--- Searching_Sorting/L2/Questions.java
+++ Searching_Sorting/L2/Questions.java
@@ -375,7 +375,26 @@ public class Questions {
     }
 
     // TODO: https://leetcode.com/problems/search-in-rotated-sorted-array-ii/ 81
-    // TODO: 153,154 => min in sorted rotaated array
+
+    //leetcode 153
+    public int findMin(int[] nums) {
+        int n = nums.length, si = 0, ei = n - 1;
+
+        while (si < ei) {
+            int mid = si + (ei - si) / 2;
+
+            if (nums[mid] < nums[ei]) {
+                // right part is sorted
+                // go left
+                ei = mid;
+            } else {
+                si = mid + 1;
+            }
+
+        }
+        return nums[si];
+    }
+    // TODO: 154 => min in sorted rotaated array
 
     // Leetcode 875 : koko and bananas
     // Approach : in this question how we think that we should use binary search:
