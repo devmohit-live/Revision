e0fe83643d260a9fcddb15c29eba897a21db9837
Mohit <mohitsingh.it5@gmail.com>
2021-11-11 17:56:18 +0530

    118 Pascal Traingle | Mohit

---
+++ Others/Daily Leetcode, Contest/PascalTraigle118.java
@@ -0,0 +1,32 @@
+public class PascalTraingel118 {
+    // CREATING PASCAL TRIANGE
+
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> row = null, prev = null;
+
+        if (numRows == 0)
+            return ans;
+
+        for (int i = 0; i < numRows; i++) {
+            row = new ArrayList<>();
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i)
+                    row.add(1);
+                else
+                    row.add(prev.get(j - 1) + prev.get(j));
+            }
+
+            prev = row;
+            ans.add(row);
+
+        }
+
+        return ans;
+    }
+
+    // Getting nth row of pascal trangle
+
+    // getting specif r,c data from pascal triangle
+
+}
