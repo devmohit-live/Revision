7139916a0ec7d538b4a646dbe99c5bedf2bdb444
Mohit <mohitsingh.it5@gmail.com>
2021-11-14 20:10:41 +0530

    First negative element in window of size k| Mohit

--- Arrays And String/Sliding Window/FixedSize.java
+++ Arrays And String/Sliding Window/FixedSize.java
@@ -1,6 +1,6 @@
 public class FixedSize {
 
-    // 1.
+    // 1. Max sum in subarray of size k
     // https://practice.geeksforgeeks.org/problems/max-sum-subarray-of-size-k5313/1#
     static int maximumSumSubarray(int k, ArrayList<Integer> arr, int n) {
         int si = 0, ei = 0, sum = 0, max = Integer.MIN_VALUE;
@@ -15,5 +15,32 @@ public class FixedSize {
         }
 
         return max;
+    }
+
+    // 2. First negative element in window of size k
+    // https://practice.geeksforgeeks.org/problems/first-negative-integer-in-every-window-of-size-k3345/1#
+    public long[] printFirstNegativeInteger(long arr[], int n, int k) {
+        long[] ans = new long[n - k + 1];
+        LinkedList<Long> q = new LinkedList<>();
+        int ei = 0, si = 0, idx = 0;
+        for (int i = 0; i < k; i++) {
+            if (arr[ei] < 0)
+                q.add(arr[ei]);
+            ei++;
+        }
+        ans[idx++] = q.size() == 0 ? 0 : q.peek();
+        while (ei < n) {
+            if (arr[si] < 0)
+                q.remove();
+            si++;
+            if (arr[ei] < 0)
+                q.add(arr[ei]);
+            ei++;
+
+            ans[idx++] = q.size() == 0 ? 0 : q.peek();
+
+        }
+        q = null;
+        return ans;
     }
 }
