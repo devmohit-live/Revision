922b7b668c74a1e0176feca5006700ed92fb56d9
Mohit <mohitsingh.it5@gmail.com>
2021-11-17 18:15:05 +0530

    Variable length slw template | Mohit

--- Arrays And String/Sliding Window/VariableSize.java
+++ Arrays And String/Sliding Window/VariableSize.java
@@ -2,8 +2,40 @@ public class VariableSize {
 
     // Lengfth of longest subarray with sun = k;
 
+    // Pattern
+
+    public int maxLength(int[] arr, int sum){
+        int  n=arr.length, si =0, ei =0,max =-1, ssf =0;
+
+        while(ei<n){
+            //operation
+            sum += arr[ei];
+            // less than the given condition
+            if(ssf< sum){
+                // keep on increasing the window
+                ei++;
+            }
+            // equals
+            else if(ssf == sum){
+                // update the ans 
+                max = Math.max(max, ei -si +1);
+            }
+            // greater
+            else if(ssf > sum){
+                while(si< n && ssf> sum){
+                    // remove the opeartion 
+                    sum -= arr[si];
+                    si++;
+                }
+                ei++; // remember to do so;
+            }
+
+        }
+        return max;
+    }
+
     // this approach works only with +ve numbers if array contains negative numbers
-    // we may have ti use kdane
+    // we may have to use kdane
 
     // Longest K unique character substring
     public int longestkSubstr(String s, int k) {
