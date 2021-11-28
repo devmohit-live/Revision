365ce0741b7280222ca28221eb5f170a9da8ca86
Mohit <mohitsingh.it5@gmail.com>
2021-11-28 19:24:50 +0530

    kth Permutaiton | Mohit

---
+++ Others/KthPermutaions.java
@@ -0,0 +1,39 @@
+public class KthPermutaions {
+
+    // IDEA: reduce the problem from finding the kth permutaion from k! to finding
+    // the permutaion's correct group first according to the first character
+    public String getPermutation(int n, int k) {
+        StringBuilder sb = new StringBuilder();
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        k = k - 1; // 0 based indexing in splitted sets
+        // factorial of previos number to find the splitted set size;
+
+        // everytime the no of groups will be n-1 ! : A
+        int fact = 1;
+        for (int i = 2; i < n; i++)
+            fact *= i;
+
+
+        // set to lookup
+        for (int i = 1; i <= n; i++)
+            arr.add(i);
+       
+        while (true) {
+            // find the index
+            int lookup = k / fact;
+            sb.append(arr.get(lookup));
+            // remove the element that is being added
+            arr.remove(lookup);
+
+            if (arr.size() == 0)
+                break;
+            // update the k
+            k %= fact; // UPDATED SEQ NUMBER WE HAVE TO FIND IN NEW GROUP SIZE
+            // size is reduced by 1 so fact=is divided by n(current size of set)
+            fact /= arr.size(); // basically calulating the n-1! =>? N! / N => N-1!
+        }
+
+        return sb.toString();
+    }
+}
