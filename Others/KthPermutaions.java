6384ab925f0df4571f8ce5d0976b893f991f7490
Mohit <mohitsingh.it5@gmail.com>
2021-11-28 22:55:18 +0530

    M Coloring Graph | Mohit

--- Others/KthPermutaions.java
+++ Others/KthPermutaions.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class KthPermutaions {
 
     // IDEA: reduce the problem from finding the kth permutaion from k! to finding
---
+++ Others/Questions/Graph/MColoring.java
@@ -0,0 +1,39 @@
+package Questions.Graph;
+
+import java.util.List;
+
+public class MColoring {
+    public static boolean graphColoring(List<Integer>[] G, int[] color, int i, int C) {
+        int n = G.length;
+        return isPossibleToColor(G, i, n, color, C);
+    }
+
+    private static boolean isPossibleToColor(List<Integer>[] G, int src, int n, int[] colors, int M) {
+        if (src == n)
+            return true; // we have reached the end node after marking all node within m colors(included)
+
+        // try all colors on this node
+        for (int i = 1; i <= M; i++) {
+            if (isSafe(G, colors, i, src)) {
+                colors[src] = i; // mark
+                // ask to next node
+                boolean rec = isPossibleToColor(G, src + 1, n, colors, M);
+                if (rec)
+                    return true;
+                // else
+                colors[src] = 0; // umark
+            }
+        }
+
+        return false;
+    }
+
+    private static boolean isSafe(List<Integer>[] G, int[] colors, int color, int node) {
+        for (int nbr : G[node]) {
+            if (colors[nbr] == color)
+                return false;
+        }
+
+        return true;
+    }
+}
