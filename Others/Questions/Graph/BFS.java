468c442186d13d07c64760ff6a3ff6cfa70d6e92
Mohit <mohitsingh.it5@gmail.com>
2021-12-04 20:56:42 +0530

    BFS: Rotten Oraanges 994 | Mohit

--- Others/Questions/Graph/BFS.java
+++ Others/Questions/Graph/BFS.java
@@ -60,7 +60,52 @@ public class BFS {
         }
     }
 
-    // Rotten Oranges:
+    // Rotten Oranges: 994
+    int[][] dir = { {0,-1} , {0,1} ,{-1,0} ,{1,0} };
     
+    public int orangesRotting(int[][] grid) {
+        int n = grid.length, m = grid[0].length, fresh = 0;
+        LinkedList<Integer> q= new LinkedList<>();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j] == 1) fresh++;
+                else if(grid[i][j] == 2){
+                    grid[i][j] = 2; //mark : justy to remember we have to  mark true
+                    q.addLast(i*m + j);
+                }
+            }
+        }
+        if(fresh == 0) return 0;
+        
+        // bfs without cycle
+        int time = 0;
+        while(!q.isEmpty()){
+            int size = q.size();
+            
+            while(size-->0){
+                int rm_idx = q.removeFirst();
+                int sr = rm_idx / m;
+                int sc = rm_idx % m;
+                
+                for(int[] d: dir){
+                    int r = sr + d[0];
+                    int c = sc + d[1];
+                    
+                    if(r>=0 && c>=0 && r<n && c<m && grid[r][c] == 1){
+                        grid[r][c] = 2; //mark true/rotten
+                        q.addLast(r*m +c);
+                        fresh--; // we are only going to fresh oranges
+                    }
+                    if(fresh == 0) return time +1;
+                }
+                
+            }
+            
+            time++;
+            
+        }
+        
+        return -1; // not possible to rott all oranges
+    }
 
 }
