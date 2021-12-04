538b7960b2ad568ffbc1c2c3e7ec2a68a5e1b403
Mohit <mohitsingh.it5@gmail.com>
2021-12-05 01:28:59 +0530

    BFS: Bipartite 785 | Mohit

--- Others/Questions/Graph/BFS.java
+++ Others/Questions/Graph/BFS.java
@@ -61,53 +61,55 @@ public class BFS {
     }
 
     // Rotten Oranges: 994
-    int[][] dir = { {0,-1} , {0,1} ,{-1,0} ,{1,0} };
-    
+    int[][] dir = { { 0, -1 }, { 0, 1 }, { -1, 0 }, { 1, 0 } };
+
     public int orangesRotting(int[][] grid) {
         int n = grid.length, m = grid[0].length, fresh = 0;
-        LinkedList<Integer> q= new LinkedList<>();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j] == 1) fresh++;
-                else if(grid[i][j] == 2){
-                    grid[i][j] = 2; //mark : justy to remember we have to  mark true
-                    q.addLast(i*m + j);
+        LinkedList<Integer> q = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1)
+                    fresh++;
+                else if (grid[i][j] == 2) {
+                    grid[i][j] = 2; // mark : justy to remember we have to mark true
+                    q.addLast(i * m + j);
                 }
             }
         }
-        if(fresh == 0) return 0;
-        
+        if (fresh == 0)
+            return 0;
+
         // bfs without cycle
         int time = 0;
-        while(!q.isEmpty()){
+        while (!q.isEmpty()) {
             int size = q.size();
-            
-            while(size-->0){
+
+            while (size-- > 0) {
                 int rm_idx = q.removeFirst();
                 int sr = rm_idx / m;
                 int sc = rm_idx % m;
-                
-                for(int[] d: dir){
+
+                for (int[] d : dir) {
                     int r = sr + d[0];
                     int c = sc + d[1];
-                    
-                    if(r>=0 && c>=0 && r<n && c<m && grid[r][c] == 1){
-                        grid[r][c] = 2; //mark true/rotten
-                        q.addLast(r*m +c);
+
+                    if (r >= 0 && c >= 0 && r < n && c < m && grid[r][c] == 1) {
+                        grid[r][c] = 2; // mark true/rotten
+                        q.addLast(r * m + c);
                         fresh--; // we are only going to fresh oranges
                     }
-                    if(fresh == 0) return time +1;
+                    if (fresh == 0)
+                        return time + 1;
                 }
-                
+
             }
-            
+
             time++;
-            
+
         }
-        
+
         return -1; // not possible to rott all oranges
     }
-
 
     // Shortest Path in Binary Matrix : 1091 (same as rotten oranges)
     int[][] dir8 = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 }, { -1, -1 }, { 1, 1 }, { 1, -1 }, { -1, 1 } };
@@ -144,4 +146,73 @@ public class BFS {
 
         return -1;
     }
+
+    // Bipartite: Possible to divide vtces in two mutually exclusive and exhaustive
+    // sets
+    // non cyclic: Bipartite
+    // cyclic (even length): Bipartite, odd length(non - bipartite)
+
+    // Leetcode 785
+    public boolean isBipartite(int[][] graph) {
+        int n = graph.length;
+        int[] vis = new int[n];
+        Arrays.fill(vis, -1);
+        boolean res = true;
+        for (int i = 0; i < n; i++) {
+            if (vis[i] == -1)
+                res = res && isComponentBipartite(graph, i, vis);
+        }
+        return res;
+    }
+
+    public boolean isComponentBipartite(int[][] graph, int src, int[] vis) {
+        LinkedList<Integer> q = new LinkedList<>();
+        q.addLast(src);
+
+        boolean isCyclic = false, isBipartite = true;
+        int color = 0; // level modified
+        while (q.size() != 0) {
+            int size = q.size();
+
+            while (size-- > 0) {
+                int rm = q.removeFirst();
+
+                if (vis[rm] != -1) {
+                    isCyclic = true;
+                    if (vis[rm] != color) { // conflict
+                        isBipartite = false;
+                        break; // no need to go further
+                    }
+                }
+
+                vis[rm] = color; // mark with color
+                for (int v : graph[rm]) {
+                    if (vis[v] == -1) { // not visited
+                        q.addLast(v);
+                    }
+                }
+
+            }
+
+            if (!isBipartite)
+                break;
+            color = (color + 1) % 2;
+        }
+
+        if (isCyclic) {
+            if (isBipartite) {
+                System.out.println("Compnenet is Bipartite and contains even length Cycle");
+            } else {
+                System.out.println("Compnenet is Non-Bipartite and contains odd length Cycle");
+            }
+        } else {
+            System.out.println("Compnenet is Bipartite.");
+        }
+        return isBipartite;
+    }
+    
+    // Leetcode 886 : Possible Bipartition
+
+    // Walls And Gates : https://www.lintcode.com/problem/663/ : Leetcode 286
+
 }
