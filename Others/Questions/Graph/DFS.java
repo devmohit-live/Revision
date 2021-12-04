356cd08b04c0267944c946e2e5c2ced0c6626084
Mohit <mohitsingh.it5@gmail.com>
2021-12-04 19:27:10 +0530

    DFS: Count Sub- Islands | Mohit

--- Others/Questions/Graph/DFS.java
+++ Others/Questions/Graph/DFS.java
@@ -11,6 +11,7 @@ public class DFS {
     StringBuilder sb;
     HashSet<String> set;
 
+    // Time: m*n, space: m*n : StringBuilder can be on m*n(all elemets are 1)
     public int numberofDistinctIslands(int[][] grid) {
         n = grid.length;
         m = grid[0].length;
@@ -40,5 +41,48 @@ public class DFS {
             }
         }
         sb.append('b'); // backtrack : this is the one that will differentate the strings
+    }
+
+    // Leetcodeb 1905 : Count sub islands
+    // NOTE: the gird 2 caontains the possible sub-islands of grid1 :
+    // if complete island from grid2 must be a prst of a island from grid1
+    // the grid2 specific part can't be a subisland : the complete island from grid2
+    // must be a part of grid1
+
+    public int countSubIslands(int[][] grid1, int[][] grid2) {
+        n = grid2.length;
+        m = grid2[0].length;
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid2[i][j] == 1) {
+                    count += dfs(grid1, grid2, i, j) ? 1 : 0;
+                }
+            }
+        }
+        return count;
+    }
+
+    private boolean dfs(int[][] grid1, int[][] grid2, int sr, int sc) {
+        // will check for complete island first and then decidde for my decision
+
+        // Remeber res && dfs() -> wont make a call if res is false, but
+        // res = dfs() && res would make a call first and we have to traverse to whole
+        // island of grid2=> to mark either in between there is water(island is broken :
+        // not a sub-island)
+
+        grid2[sr][sc] = 2;
+        boolean res = true;
+        for (int[] d : dir) {
+            int r = sr + d[0];
+            int c = sc + d[1];
+
+            if (r >= 0 && c >= 0 && r < n && c < m && grid2[r][c] == 1) {
+                res = dfs(grid1, grid2, r, c) && res;
+            }
+
+        }
+
+        return res && grid1[sr][sc] == 1; // my decision if grid1 contains 1 at sr sc or ot
     }
 }
