31abe0ad11a7db09693a2fd6aa5ca98a960d5923
Mohit <mohitsingh.it5@gmail.com>
2021-12-06 22:02:08 +0530

    DSU: 990 Satisfiability of Equality Equations | Mohit

--- Others/Questions/Graph/dsuQuestions.java
+++ Others/Questions/Graph/dsuQuestions.java
@@ -1,8 +1,11 @@
 package Questions.Graph;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class dsuQuestions {
     // 695 : Max Area of Islands:
-
+    int[] parent, size;
     int[][] dir = { { 0, 1 }, { 1, 0 } };
 
     public int maxAreaOfIsland(int[][] grid) {
@@ -34,6 +37,7 @@ public class dsuQuestions {
                         int r = i + d[0], c = j + d[1];
                         if (r >= 0 && c >= 0 && r < n && c < m && grid[r][c] == 1) {
                             int p2 = findParent(r * m + c);
+                            // merge : union
                             if (p1 != p2) {
                                 parent[p2] = p1;
                                 size[p1] += size[p2];
@@ -60,6 +64,55 @@ public class dsuQuestions {
         if (parent[x] == x)
             return x;
         return parent[x] = findParent(parent[x]);
+    }
+
+    // Leetcode 990 : Satisfiability of Equality Equations
+    public boolean equationsPossible(String[] equations) {
+        // initilization
+        int[] parent = new int[26];
+        // int[] size = new int[26];
+        for (int i = 0; i < 26; i++) {
+            parent[i] = i;
+            // size[] = 1;
+        }
+
+        List<String> notEqual = new ArrayList<>();
+        // perform all == => union op first
+        // then start cheking for != conditions
+
+        boolean res = true;
+        for (String eq : equations) {
+            char a = eq.charAt(0);
+            char b = eq.charAt(eq.length() - 1);
+            char condition = eq.charAt(1);
+            int p1 = findParent(parent, a - 'a');
+            int p2 = findParent(parent, b - 'a');
+
+            if (condition == '=') {
+                // union
+                parent[p2] = p1;
+            } else {
+                notEqual.add(eq);
+            }
+
+        }
+
+        for (String s : notEqual) {
+            char a = s.charAt(0);
+            char b = s.charAt(s.length() - 1);
+            int p1 = findParent(parent, a - 'a');
+            int p2 = findParent(parent, b - 'a');
+            if (p1 == p2)
+                return false;
+        }
+
+        return res;
+    }
+
+    private int findParent(int[] parent, int x) {
+        if (parent[x] == x)
+            return x;
+        return parent[x] = findParent(parent, parent[x]);
     }
 
 }
