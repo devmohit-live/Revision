2fb185440cd13c4339683c395fd762b7101fc2eb
Mohit <mohitsingh.it5@gmail.com>
2021-12-07 12:01:08 +0530

    Top K freq words | Mohit

---
+++ Others/Questions/AMZ/TopKFrequent.java
@@ -0,0 +1,33 @@
+package Questions.AMZ;
+
+public class TopKFrequent {
+    public List<String> topKFrequent(String[] words, int k) {
+        HashMap<String, Integer> map = new HashMap<>();
+        PriorityQueue<String> pq = new PriorityQueue<>((a, b) -> {
+            int f1 = map.get(a), f2 = map.get(b);
+            if (f1 == f2) {
+                // same freq : I havr to eliminate the lexo greater element
+                return b.compareTo(a);
+            }
+            return f1 - f2; // return the lesser elemetn
+        });
+
+        for (String word : words) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+
+        for (String key : map.keySet()) {
+            pq.add(key);
+            if (pq.size() > k) {
+                pq.remove();
+            }
+        }
+
+        // highest to lowest
+        LinkedList<String> res = new LinkedList<>();
+        while (pq.size() != 0) {
+            res.addFirst(pq.remove());
+        }
+        return (List<String>) res;
+    }
+}
