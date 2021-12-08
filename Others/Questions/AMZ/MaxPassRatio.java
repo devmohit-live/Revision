1f8a5d878f642c02229621eb504427130f890a46
Mohit <mohitsingh.it5@gmail.com>
2021-12-08 18:32:04 +0530

    Max Average Pass Ratio 1792 | Mohit

---
+++ Others/Questions/AMZ/MaxPassRatio.java
@@ -0,0 +1,51 @@
+public class MaxPassRatio {
+    // lc: 1792. Maximum Average Pass Ratio
+    class Solution {
+        class Pair {
+            double ratioIfAdded, current;
+            int passed, total;
+
+            Pair(int[] data) {
+                this.passed = data[0];
+                this.total = data[1];
+                this.ratioIfAdded = (this.passed + 1) * 1.0 / (this.total + 1);
+                this.current = (this.passed) * 1.0 / (this.total);
+            }
+        }
+
+        public double maxAverageRatio(int[][] classes, int extraStudents) {
+            int n = classes.length;
+            double ans = 0; // average growth sum
+
+            // will give the class that will cause max growth on addition
+            PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> {
+                double growthA = a.ratioIfAdded - a.current;
+                double growthB = b.ratioIfAdded - b.current;
+
+                return Double.compare(growthB, growthA);
+            });
+
+            for (int i = 0; i < n; i++) {
+                Pair pair = new Pair(classes[i]);
+                pq.add(pair);
+            }
+
+            while (extraStudents-- > 0) {
+                Pair rm = pq.remove();
+                // add the pair after modification
+                int passed = rm.passed + 1, total = rm.total + 1;
+
+                pq.add(new Pair(new int[] { passed, total }));
+
+            }
+
+            while (pq.size() > 0) {
+                Pair rm = pq.remove();
+                ans += rm.current;
+            }
+
+            return ans / n; // net avg growth
+        }
+
+    }
+}
