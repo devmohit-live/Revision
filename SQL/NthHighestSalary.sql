d72c40d6a4925117157c3b5f84cff4f210bcaa70
Mohit <mohitsingh.it5@gmail.com>
2021-12-15 12:32:59 +0530

    Nth highest salary using limit and offset | Mohit

--- SQL/NthHighestSalary.sql
+++ SQL/NthHighestSalary.sql
@@ -11,3 +11,17 @@ END
 -- # Using dense ranking
 
 -- # Using subquery
+-- Using offset and limit
+-- offset : skip starting k rows
+--  limit take only k rows 
+--  approach : sort them by desc order of their salay (distinct as salary can be reapeated/same) : offset n-1, limit 1
+--  at start use set N = N -1 (can't to that in query) : first limit is writtenn then offset
+
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  set N = N -1;
+  RETURN (
+      # Write your MySQL query statement below.
+      select distinct salary from employee order by salary desc limit 1  offset N
+  );
+END
