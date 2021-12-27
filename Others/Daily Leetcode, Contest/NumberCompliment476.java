b5b432a908c9655058819f771870cc72b6f92ca8
Mohit <mohitsingh.it5@gmail.com>
2021-12-27 08:28:45 +0530

    LC 476 | Mohit

--- Others/Daily Leetcode, Contest/NumberCompliment476.java
+++ Others/Daily Leetcode, Contest/NumberCompliment476.java
@@ -1,13 +1,17 @@
 class NumberCompliment476{
-     if(num == 0) return 1;
+    public int findComplement(int num) {
+        if (num == 0)
+            return 1;
         // no of bits in which this number can be represented
         int nBits = (int) Math.floor((Math.log(num) / Math.log(2)) + 1);
-        // or Integer.highestOneBit(n) 
-        
+        // or Integer.highestOneBit(n)
+
         // actal compliment
         int comp = (~num);
-        // mask : to set the intitial 1's of the complement(which causes problem)(32 - nbiits)
+        // mask : to set the intitial 1's of the complement(which causes problem)(32 -
+        // nbiits)
         // ex: num = 5, 101=>3 bits (1<<3) => 1000 -1 =>0111 => 00000....0111
         int mask = (1 << nBits) - 1;
         return (comp & mask);
+    }
 }
