e321fe7d7a9f9a32cad4acccabea78adff124373
Mohit <mohitsingh.it5@gmail.com>
2022-01-16 23:44:22 +0530

    Binary tree to DLL | Mohit

---
+++ BT/L2/Converions.java
@@ -0,0 +1,32 @@
+public class Converions {
+    // Binary tree to DLL :
+    // https://practice.geeksforgeeks.org/problems/binary-tree-to-dll/1#
+
+    // .. Approach 1 (Recursive): Using static => can be removed by using arr of node of size 1/ treeNode.right propertyu: heap
+
+    Node prev = null;
+
+    Node bToDLL(Node root) {
+        Node dummy = new Node(-1);
+        prev = dummy;
+
+        bToDLL_(root);
+        Node head = dummy.right;
+        head.left = null;
+        return head;
+    }
+
+    private void bToDLL_(Node root) {
+        if (root == null)
+            return;
+        // inorder
+        bToDLL_(root.left);
+        root.left = prev;
+        prev.right = root;
+        prev = root;
+
+        bToDLL_(root.right);
+    }
+
+    // Ditto same: CIrcular DLL : just attach head to tail
+}
