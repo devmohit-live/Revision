101cb8a1b1e653540cac8748b552c677b35c54fd
Mohit <mohitsingh.it5@gmail.com>
2022-01-16 22:48:44 +0530

    VerticalOrderBT LC 987 approach using 1 pq | Mohit

--- BT/L2/AscVerticalOrder.java
+++ BT/L2/AscVerticalOrder.java
@@ -1,5 +1,7 @@
 package Questions.Trees;
 
+import java.util.PriorityQueue;
+
 public class AscVerticalOrder {
 
     // Leetcode 987. Vertical Order Traversal of a Binary Tree
@@ -7,13 +9,30 @@ public class AscVerticalOrder {
 
     class vpair {
         TreeNode node;
-        int vlevel;
+        int vlevel, level = 0;
 
-        vpair(TreeNode node, int vlevel) {
+        // for second approach
+        vpair(TreeNode node, int vlevel, int hlevel) {
             this.node = node;
             this.vlevel = vlevel;
+            this.level = hlevel;
+        }
+
+        vpair(TreeNode node, int vlevel) {
+            this(node, vlevel, 0);
         }
     }
+
+    private static void vminmax(TreeNode root, int level, int[] minmax) {
+        if (root == null)
+            return;
+        minmax[0] = Math.min(minmax[0], level);
+        minmax[1] = Math.max(minmax[1], level);
+
+        vminmax(root.left, level - 1, minmax);
+        vminmax(root.right, level + 1, minmax);
+    }
+    // Two pq: to ensure the levels which we were doing with the help of size in q
 
     public List<List<Integer>> verticalTraversal(TreeNode root) {
         int[] minmax = new int[2];
@@ -65,14 +84,45 @@ public class AscVerticalOrder {
         return ans;
     }
 
-    private static void vminmax(TreeNode root, int level, int[] minmax) {
-        if (root == null)
-            return;
-        minmax[0] = Math.min(minmax[0], level);
-        minmax[1] = Math.max(minmax[1], level);
+    public List<List<Integer>> verticalTraversalBetter(TreeNode root) {
+        int[] minmax = new int[2];
+        vminmax(root, 0, minmax);
+        int length = minmax[1] - minmax[0] + 1;
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < length; i++)
+            ans.add(new ArrayList<>());
 
-        vminmax(root.left, level - 1, minmax);
-        vminmax(root.right, level + 1, minmax);
-    }
+        PriorityQueue<vpair> pq = new PriorityQueue<>((a, b) -> {
+            // first differentite on basis of level : hlevel ( simulating the child pq:
+            // first priority)
+            if (a.level == b.level) {
+                // diffrentitate of vlevel
+                if (a.vlevel == b.vlevel)
+                    return a.node.val - b.node.val;
+                return a.vlevel - b.vlevel;
+            }
+            return a.level - b.level;
+        });
+
+        pq.add(new vpair(root, Math.abs(minmax[0]), 0));
+        while (pq.size() > 0) {
+            int size = pq.size();
+            while (size-- > 0) {
+                vpair rm = pq.remove();
+                int vl = rm.vlevel, hl = rm.level;
+                TreeNode rmnode = rm.node;
+                ans.get(vl).add(rmnode.val);
+
+                if (rmnode.left != null)
+                    pq.add(new vpair(rmnode.left, vl - 1, hl + 1));
+                if (rmnode.right != null)
+                    pq.add(new vpair(rmnode.right, vl + 1, hl + 1));
+            }
+
+        }
+        return ans;
+
+      }
+
 
 }
