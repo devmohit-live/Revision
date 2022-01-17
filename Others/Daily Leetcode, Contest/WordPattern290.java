3fba919d4c21eb0dae15a6908e4626269e571b90
Mohit <mohitsingh.it5@gmail.com>
2022-01-17 12:54:53 +0530

    Word Pattern Using Refrence Property of Objects  LC:  290 | Mohit

--- Others/Daily Leetcode, Contest/WordPattern290.java
+++ Others/Daily Leetcode, Contest/WordPattern290.java
@@ -35,5 +35,48 @@ public class WordPattern290 {
         return true;
     }
 
+    // Using help of Object Refrence:
+
+    public boolean wordPattern_Ref(String pattern, String s) {
+        String[] words = s.split(" ");
+        if (words.length != pattern.length())
+            return false;
+
+        Map map = new HashMap<>();
+        // a generic hashmap which can have any type of key and values
+        // purpose: let : char or string, val => index: refrece object acting as index
+        // storing the Integer(index actually : refrence of Integer object)
+        // == matches refrence .equlas()=> value so we are matching refrences
+
+        // map.put(a,b) : return previousl values of a if any else null
+        for (Integer i = 0; i < words.length; i++) {
+            if (map.put(pattern.charAt(i), i) != map.put(words[i], i))
+                return false;
+        }
+
+        return true;
+    }
+
+    // Points to remeber:
+    // Resource : https://
+    // leetcode.com/problems/word-pattern/discuss/73402/8-lines-simple-Java
+    /*
+     * Just want to point out one thing about autoboxing. As mentioned
+     * by @StefanPochmann, we can try such an example:
+     * 
+     * int i = 10; Integer a = i; Integer b = i; System.out.println(a == b); //guess
+     * what is the output? The output was supposed to be false. However, you can
+     * test this and you will find it is true. Why?
+     * 
+     * Because
+     * "The JVM is caching Integer values. == only works for numbers between -128 and 127 "
+     * Then you can try another code:
+     * 
+     * int i = 1000; //greater than 127 Integer a = i; Integer b = i;
+     * System.out.println(a == b); //this time we got false Look, now you get false.
+     * And now it explains why we can pass the small cases (because the indices are
+     * in the range of -128 and 127). We also know why it cannot pass the larger
+     * test case.
+     */
 
 }
