8934bc9e009a1aa3bc8ef19b873ef5ef8a881dda
Mohit <mohitsingh.it5@gmail.com>
2022-01-21 14:59:43 +0530

    Greedy : Gas Station LC 134 One Pass| Mohit

--- Others/Questions/Greedy/GasStation134.java
+++ Others/Questions/Greedy/GasStation134.java
@@ -4,7 +4,7 @@
 public class GasStation134 {
 
     // Approach1: checking first if even it is possible or not then aaplying greedy
-    // O(n)
+    // O(2n)
     public int canCompleteCircuit(int[] gas, int[] cost) {
         int n = gas.length;
         int tank = 0;
@@ -27,5 +27,24 @@ public class GasStation134 {
         }
         return ans;
     }
+
+    // Approach2 : maintaining the defficient tank in meanwhile to avoid extra loop
+    // : O(n)
+    public int canCompleteCircuit2(int[] gas, int[] cost) {
+        int n = gas.length, def = 0, tank = 0, net = 0, ans = 0;
+        for (int i = 0; i < n; i++) {
+            tank += gas[i] - cost[i];
+            net += gas[i] - cost[i];
+            if (tank < 0) {
+                ans = i + 1;
+                tank = 0;
+            }
+
+        }
+
+        return net < 0 ? -1 : ans;
+    }
+
+    
 
 }
