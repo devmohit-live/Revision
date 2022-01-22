7893f03c568f365a043719a979c2b5c82d64c317
Mohit <mohitsingh.it5@gmail.com>
2022-01-22 18:51:06 +0530

    Construction : PostOrder to BST  | Mohit

--- BT/L2/Construction.java
+++ BT/L2/Construction.java
@@ -13,20 +13,21 @@ public class Construction {
         }
     }
 
-
-    // NOTE:  for addition/deletion of nodes in tree work in postorder
-
+    // NOTE: for addition/deletion of nodes in tree work in postorder
 
     // Preorder traversal to BST
     int idx = 0;
+
     public TreeNode preOrderToBST(int[] preorder) {
         // fact: root in start : but not aable to identify left and right subtree
         // so we use concept of bst range left<=root<right
         idx = 0;
         return preOrderToBST(preorder, Integer.MIN_VALUE, Integer.MAX_VALUE);
     }
-    private TreeNode preOrderToBST(int[] preorder, int left_range, int right_range){
-        if(idx>= preorder.length || preorder[idx]<left_range || preorder[idx]> right_range) return null;
+
+    private TreeNode preOrderToBST(int[] preorder, int left_range, int right_range) {
+        if (idx >= preorder.length || preorder[idx] < left_range || preorder[idx] > right_range)
+            return null;
         TreeNode root = new TreeNode(preorder[idx++]);
         root.left = preOrderToBST(preorder, left_range, root.val);
         root.right = preOrderToBST(preorder, root.val, right_range);
@@ -51,6 +52,24 @@ public class Construction {
         return root;
     }
 
-    // PostOrder traversal to BST
+    // PostOrder traversal to BST : similar to preorder just idx will start from
+    // length-1 as root is at last
+    public TreeNode postOrderToBST(int[] postorder) {
+        // fact: root in start : but not aable to identify left and right subtree
+        // so we use concept of bst range left<=root<right
+        idx = postorder.length - 1;
+        return postOrderToBST(postorder, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    private TreeNode postOrderToBST(int[] postorder, int left_range, int right_range) {
+        if (idx < 0 || postorder[idx] < left_range || postorder[idx] > right_range)
+            return null;
+        TreeNode root = new TreeNode(postorder[idx--]);
+        //NOTE: making right call first as root is at last idx and just before that we have right child
+
+        root.right = postOrderToBST(postorder, root.val, right_range);
+        root.left = postOrderToBST(postorder, left_range, root.val);
+        return root;
+    }
 
 }
