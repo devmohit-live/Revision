7b8267b61d13edf149f813e04181de04a54563ab
Mohit <mohitsingh.it5@gmail.com>
2022-01-22 18:42:13 +0530

    Construction : PreOrder to BST  | Mohit

--- BT/L2/Construction.java
+++ BT/L2/Construction.java
@@ -13,10 +13,29 @@ public class Construction {
         }
     }
 
+
+    // NOTE:  for addition/deletion of nodes in tree work in postorder
+
+
     // Preorder traversal to BST
+    int idx = 0;
+    public TreeNode preOrderToBST(int[] preorder) {
+        // fact: root in start : but not aable to identify left and right subtree
+        // so we use concept of bst range left<=root<right
+        idx = 0;
+        return preOrderToBST(preorder, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+    private TreeNode preOrderToBST(int[] preorder, int left_range, int right_range){
+        if(idx>= preorder.length || preorder[idx]<left_range || preorder[idx]> right_range) return null;
+        TreeNode root = new TreeNode(preorder[idx++]);
+        root.left = preOrderToBST(preorder, left_range, root.val);
+        root.right = preOrderToBST(preorder, root.val, right_range);
+        return root;
+    }
+
     // Inorder traversal to BST
     public TreeNode inorderToBst(int[] inorder) {
-        // fact: root in mid : bale to identify left and right subtree
+        // fact: root in mid : able to identify left and right subtree
         return inorderToBst(inorder, 0, inorder.length - 1);
     }
 
