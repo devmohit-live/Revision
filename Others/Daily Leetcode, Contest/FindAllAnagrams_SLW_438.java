20186399bd6fd3403765b74e7c34b7f014c0c55b
Mohit <mohitsingh.it5@gmail.com>
2022-02-02 12:08:48 +0530

    SLW: FindAllAnagrams_SLW_438 Approach1 : Updated| Mohit

--- Others/Daily Leetcode, Contest/FindAllAnagrams_SLW_438.java
+++ Others/Daily Leetcode, Contest/FindAllAnagrams_SLW_438.java
@@ -1,40 +1,42 @@
 class FindAllAnagrams_SLW_438 {
     public List<Integer> findAnagrams(String s, String t) {
-        List<Integer> result = new LinkedList<>();
-        if (t.length() > s.length())
-            return result;
-        Map<Character, Integer> map = new HashMap<>();
-        for (char c : t.toCharArray()) {
-            map.put(c, map.getOrDefault(c, 0) + 1);
-        }
-        int counter = map.size();
+        int si = 0, ei = 0, n = s.length(), k = p.length();
+        List<Integer> ans = new ArrayList<>();
+        if (k > n)
+            return ans;
 
-        int begin = 0, end = 0;
-        while (end < s.length()) {
-            char c = s.charAt(end);
-            if (map.containsKey(c)) {
-                map.put(c, map.get(c) - 1);
-                if (map.get(c) == 0)
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char ch : p.toCharArray())
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        int counter = map.size(); // distinct elemets in string
+
+        while (ei < n) {
+            char ch = s.charAt(ei);
+            if (map.containsKey(ch)) {
+                map.put(ch, map.get(ch) - 1);
+                if (map.get(ch) == 0)
                     counter--;
             }
-            end++;
 
             while (counter == 0) {
-                char tempc = s.charAt(begin);
-                if (map.containsKey(tempc)) {
-                    map.put(tempc, map.get(tempc) + 1);
-                    if (map.get(tempc) > 0) {
+                char rm = s.charAt(si);
+
+                if (map.containsKey(rm)) {
+                    map.put(rm, map.get(rm) + 1);
+                    if (map.get(rm) > 0)
                         counter++;
-                    }
+
                 }
-                if (end - begin == t.length()) {
-                    result.add(begin);
-                }
-                begin++;
+
+                if (ei - si + 1 == k)
+                    ans.add(si);
+                si++;
             }
 
+            ei++;
         }
-        return result;
+
+        return ans;
     }
 
     // Approach 2: Using two maps
