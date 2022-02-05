91f209ebb28f4caeb3a8b4cc9dc5533e25177589
Mohit <mohitsingh.it5@gmail.com>
2022-02-05 12:42:08 +0530

    Path Sum 2 | Mohit

--- Trees/DiamterAndQuestions.java
+++ Trees/DiamterAndQuestions.java
@@ -83,4 +83,35 @@ public class DiamterAndQuestions {
     }
 
     // LC 113: Path Sum 2
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> small = new ArrayList<>();
+
+        pathSum(root, targetSum, small, ans);
+        return ans;
+    }
+
+    private void pathSum(TreeNode node, int tar, List<Integer> small, List<List<Integer>> ans) {
+
+        if (node == null)
+            return;
+
+        if (node.left == null && node.right == null) {
+            if (tar - node.val == 0) {
+                small.add(node.val); // caluculations
+                ans.add(new ArrayList<>(small));
+                small.remove(small.size() - 1); // remove the calculation here too
+
+            }
+            return;
+        }
+
+        small.add(node.val);
+
+        pathSum(node.left, tar - node.val, small, ans);
+        pathSum(node.right, tar - node.val, small, ans);
+
+        small.remove(small.size() - 1);
+    }
+
 }
