1bbab5a55733a8242525010a61500ef3dbe6d14a
Mohit <mohitsingh.it5@gmail.com>
2022-02-14 03:09:35 +0530

    Contest 280 : Q3 Upsolve | Mohit

--- Others/Daily Leetcode, Contest/Contest280.java
+++ Others/Daily Leetcode, Contest/Contest280.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Contest280 {
     // Question 1: 2169. Count Operations to Obtain Zero
     // Approach 1 : Better O(1) space
@@ -80,7 +82,22 @@ public class Contest280 {
         return Math.min(choice1,choice2);
         
     }
-    
-    
+
+    // Question 3 : 2171. Removing Minimum Number of Magic Beans
+    public long minimumRemoval(int[] beans) {
+        Arrays.sort(beans);
+        int n = beans.length; // taking int causes a problem
+        
+        long ans = Long.MAX_VALUE,sum = 0;
+        for(int el: beans) sum+=el;
+        
+        for(int i=0;i<n;i++){
+            long changes = sum - ((n-i*1l)*beans[i]); // all number are onverted to b=beasn[i];
+            
+            ans = Math.min(ans,changes);
+        }
+        
+        return ans;
+    }
 
 }
