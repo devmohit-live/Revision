d0a806cbdd7e6c6e583577b73ee51d4d57fba506
Mohit <mohitsingh.it5@gmail.com>
2022-02-24 15:38:49 +0530

    Blind75 : Spiral Matrix | Mohit

--- 2dArray/Spiral.java
+++ 2dArray/Spiral.java
@@ -49,35 +49,80 @@ public class Spiral {
         }
     }
 
-    public static void spiralClockWise(int[][] arr) {
-        int n = arr.length, m = arr[0].length;
-        int rmin = 0, cmin = 0, rmax = n - 1, cmax = m - 1, total = m * n;
+    // //wRONFG FAILS ON SOME CASES ON LEETCODE
+    // public static void spiralClockWise(int[][] arr) {
+    // int n = arr.length, m = arr[0].length;
+    // int rmin = 0, cmin = 0, rmax = n - 1, cmax = m - 1, total = m * n;
 
-        while (total > 0) {
-            for (int itr = cmin; itr <= cmax; itr++) {
-                System.out.println(arr[rmin][itr]);
-                total--;
-            }
-            rmin++;
+    // while (total > 0) {
+    // for (int itr = cmin; itr <= cmax; itr++) {
+    // System.out.println(arr[rmin][itr]);
+    // total--;
+    // }
+    // rmin++;
 
-            for (int itr = rmin; itr <= rmax; itr++) {
-                System.out.println(arr[itr][cmax]);
-                total--;
-            }
-            cmax--;
+    // for (int itr = rmin; itr <= rmax; itr++) {
+    // System.out.println(arr[itr][cmax]);
+    // total--;
+    // }
+    // cmax--;
 
-            for (int itr = cmax; itr >= cmin; itr--) {
-                System.out.println(arr[rmax][itr]);
-                total--;
-            }
-            rmax--;
+    // for (int itr = cmax; itr >= cmin; itr--) {
+    // System.out.println(arr[rmax][itr]);
+    // total--;
+    // }
+    // rmax--;
 
-            for (int itr = rmax; itr >= rmin; itr--) {
-                System.out.println(arr[itr][cmin]);
-                total--;
+    // for (int itr = rmax; itr >= rmin; itr--) {
+    // System.out.println(arr[itr][cmin]);
+    // total--;
+    // }
+    // cmin++;
+    // }
+    // }
+
+    // CORRECT:
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new LinkedList<Integer>();
+        if (matrix == null || matrix.length == 0)
+            return result;
+
+        int startRow = 0, endRow = matrix.length - 1;
+        int startCol = 0, endCol = matrix[0].length - 1;
+        int dir = 0;
+
+        while (startRow <= endRow && startCol <= endCol) {
+            switch (dir) {
+            case 0: // LEFT TO RIGHT
+                for (int col = startCol; col <= endCol; col++)
+                    result.add(matrix[startRow][col]);
+                startRow++;
+                break;
+
+            case 1: // TOP TO DOWN
+                for (int row = startRow; row <= endRow; row++)
+                    result.add(matrix[row][endCol]);
+                endCol--;
+                break;
+
+            case 2:// RIGHT TO LEFT
+                for (int col = endCol; col >= startCol; col--)
+                    result.add(matrix[endRow][col]);
+                endRow--;
+                break;
+
+            case 3:// DOWN TO UP
+                for (int row = endRow; row >= startRow; row--)
+                    result.add(matrix[row][startCol]);
+                startCol++;
+                break;
+
             }
-            cmin++;
+
+            dir = (dir + 1) % 4;
         }
+
+        return result;
     }
 
 }
