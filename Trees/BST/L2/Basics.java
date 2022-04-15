d245c608c361712dee06b55a218401f239f03972
Mohit <mohitsingh.it5@gmail.com>
2022-04-15 10:04:04 +0530

    Trim a bst LC 669 | Mohit

--- Trees/BST/L2/Basics.java
+++ Trees/BST/L2/Basics.java
@@ -251,4 +251,28 @@ public class Basics {
 
     }
 
+    //LC 669 : trim a bst 
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if (root == null)
+            return null;
+
+        if (root.val >= low && root.val <= high) {
+            // valid tree : go fotr left and tree tree trimming
+            root.left = trimBST(root.left, low, high);
+            root.right = trimBST(root.right, low, high);
+        } else if (root.val < low) {
+            // since root is smaller than low and it is BST, so left subtree will even be
+            // smaller
+            // so no profit of calling the left, and can't even include the root as it
+            // voilates the condition
+            root = trimBST(root.right, low, high);
+        } else if (root.val > high) {
+            root = trimBST(root.left, low, high);
+        }
+
+        return root;
+    }
+
+
+
 }
