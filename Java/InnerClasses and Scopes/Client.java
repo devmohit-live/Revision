12cdd0880f4d7c83b68570f3824eb7ebe3c8718b
Mohit <mohitsingh.it5@gmail.com>
2022-05-02 11:37:50 +0530

    Java : Inner class variable scopes | Mohit

--- Java/InnerClasses and Scopes/Client.java
+++ Java/InnerClasses and Scopes/Client.java
@@ -9,5 +9,10 @@ public class Client {
         // can't do that : an enclosing instance that contains Outer.Inner is required
         // Outer.Inner inner2 = new Outer.Inner();
         // inner2.accessOuterData();
+
+        // cant' access private var directly by inner objeect though
+        System.out.println(innerclass.innervar1);
+        // System.out.println(innerclass.innervar2); will give error as it is a private var
+
     }
 }
