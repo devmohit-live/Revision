c4852d1018500ae43242b6bf7410d2337c453e7f
Mohit <mohitsingh.it5@gmail.com>
2022-05-03 11:50:35 +0530

    Concurrency In Java : LC 1114 : Using Volatile | Mohit

--- Others/LC Multithreading Questions/PrintInOrder_1114.java
+++ Others/LC Multithreading Questions/PrintInOrder_1114.java
@@ -1,5 +1,5 @@
 public class PrintInOrder_1114 {
-    //Using CountDownLatch : easisest
+    // Using CountDownLatch : easisest
     class Foo_UsingCountDownLatch {
         // https://stackoverflow.com/questions/8579657/whats-the-difference-between-thread-start-and-runnable-run
 
@@ -32,9 +32,44 @@ public class PrintInOrder_1114 {
         }
     }
 }
-// Different ways: 
+// Different ways:
 // https://leetcode.com/problems/print-in-order/discuss/893827/Java-SynchronizedLockSemaphoreCondition-Variable
 
-
 // Using Volatile keyword
+class Foo_UsingVolatile {
+    private volatile int counter; // counter will be different for differnt object of class
+    // so if you want to have some kind of synchro on the object basis this is
+    // preferred
 
+    // private static volatile int counter;
+    // if you want synchro on class bases this is preferred : very rare case :
+    // singlewton classex
+
+    public Foo() {
+        this.counter = 0;
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        printFirst.run();
+        counter = 1;
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        while (counter < 1)
+            ;
+
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        printSecond.run();
+        counter = 2;
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+
+        while (counter < 2)
+            ;
+        // printThird.run() outputs "third". Do not change or remove this line.
+        printThird.run();
+    }
+}
