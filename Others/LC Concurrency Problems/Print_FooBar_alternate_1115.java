cf921ca40734f045fb303fcda9e30d5ba1346084
Mohit <mohitsingh.it5@gmail.com>
2022-05-04 11:57:42 +0530

    MultiThreading : Print FooBar Alternatively LC 1115 : Using notify and wait| Mohit

--- Others/LC Concurrency Problems/Print_FooBar_alternate_1115.java
+++ Others/LC Concurrency Problems/Print_FooBar_alternate_1115.java
@@ -1,8 +1,8 @@
-class FooBar {
+class FooBar_UsingSemaphore {
     private int n;
     Semaphore foo, bar;
 
-    public FooBar(int n) {
+    public FooBar_UsingSemaphore(int n) {
         this.n = n;
         this.foo = new Semaphore(1); // starting point
         this.bar = new Semaphore(0);
@@ -27,3 +27,57 @@ class FooBar {
         }
     }
 }
+
+
+//Using Synchro and wait notify
+class FooBar_UsingWaitNotigyAndSynchro{
+    private int n;
+    private boolean runFoo;
+
+    public FooBar_UsingWaitNotigyAndSynchro(int n) {
+        this.n = n;
+        this.runFoo = true;
+    }
+
+    public void foo(Runnable printFoo) throws InterruptedException {
+
+        for (int i = 0; i < n; i++) {
+
+            synchronized (this) {
+                while (!runFoo) {
+                    wait();
+                }
+
+                printFoo.run();
+                runFoo = false;
+                notifyAll();
+
+            }
+
+        }
+    }
+
+    public void bar(Runnable printBar) throws InterruptedException {
+
+        for (int i = 0; i < n; i++) {
+
+            synchronized (this) {
+                while (runFoo) {
+                    wait();
+                }
+                printBar.run();
+                runFoo = true;
+                notifyAll();
+
+            }
+
+        }
+
+    }
+}
+
+//Using Retreant Lock and Contions with volatile : faster than synchro block
+//Retrant : use to acquire lock
+//conditions: used to send awwait and signal signals
+// volatile variable : for use according to the problems statements req : ex print alternate
+
