8764f4aa3738534655870c7bc4c988050670b51f
Mohit <mohitsingh.it5@gmail.com>
2022-05-06 20:55:10 +0530

    Concurreny : 1116 Zero Even Odd : Using Semaphores | Mohit

--- Others/LC Concurrency Problems/PrintZeroEvenOdd_1116.java
+++ Others/LC Concurrency Problems/PrintZeroEvenOdd_1116.java
@@ -1,3 +1,4 @@
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -136,6 +137,50 @@ public class PrintZeroEvenOdd_1116 {
                     lock.unlock();
                 }
             }
+        }
+    }
+
+}
+
+class ZeroEvenOdd_Using_Semaphores {
+    private int n;
+    private Semaphore zero, even, odd;
+
+    public ZeroEvenOdd_Using_Semaphores(int n) {
+        this.n = n;
+        this.zero = new Semaphore(1);
+        this.even = new Semaphore(0);
+        this.odd = new Semaphore(0);
+    }
+
+    // printNumber.accept(x) outputs "x", where x is an integer.
+    public void zero(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 1; i <= n; i++) {
+            zero.acquire();
+
+            printNumber.accept(0);
+            if (i % 2 == 0) {
+                even.release();
+            } else {
+                odd.release();
+            }
+
+        }
+    }
+
+    public void even(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 2; i <= n; i += 2) {
+            even.acquire();
+            printNumber.accept(i);
+            zero.release();
+        }
+    }
+
+    public void odd(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 1; i <= n; i += 2) {
+            odd.acquire();
+            printNumber.accept(i);
+            zero.release();
         }
     }
 
