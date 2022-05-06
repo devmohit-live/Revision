61b0f1d5f57fbd8457e9efcbfbc1f01293055020
Mohit <mohitsingh.it5@gmail.com>
2022-05-06 20:45:57 +0530

    Concurreny : 1116 Zero Even Odd : Using Wait,NotifyAll with syncronized block and volatile | Mohit

--- Others/LC Concurrency Problems/PrintZeroEvenOdd_1116.java
+++ Others/LC Concurrency Problems/PrintZeroEvenOdd_1116.java
@@ -3,6 +3,65 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.function.IntConsumer;
 
+class ZeroEvenOdd_UsingWait_NotifyAll_synchro_volatile {
+    private int n;
+    private volatile int currState;
+    private final int ZERO, EVEN, ODD;
+
+    public ZeroEvenOdd_UsingWait_NotifyAll_synchro_volatile(int n) {
+        this.n = n;
+        this.ZERO = 0;
+        this.EVEN = 2;
+        this.ODD = 1;
+        this.currState = ZERO;
+    }
+
+    // printNumber.accept(x) outputs "x", where x is an integer.
+    public void zero(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 1; i <= n; i++) {
+            synchronized (this) {
+                while (currState != ZERO)
+                    wait();
+
+                printNumber.accept(0);
+                if ((i & 1) == 1) {
+                    currState = ODD;
+                } else {
+                    currState = EVEN;
+                }
+
+                notifyAll();
+            }
+        }
+    }
+
+    public void even(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 2; i <= n; i += 2) {
+            synchronized (this) {
+                while (currState != EVEN)
+                    wait();
+
+                printNumber.accept(i);
+                currState = ZERO;
+                notifyAll();
+            }
+        }
+    }
+
+    public void odd(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 1; i <= n; i += 2) {
+            synchronized (this) {
+                while (currState != ODD)
+                    wait();
+
+                printNumber.accept(i);
+                currState = ZERO;
+                notifyAll();
+            }
+        }
+    }
+}
+
 public class PrintZeroEvenOdd_1116 {
 
     class ZeroEvenOdd_UsingReentrantLock_volatile {
