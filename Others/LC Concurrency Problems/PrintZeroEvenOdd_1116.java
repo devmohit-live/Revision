c0d151b3f71de0b446aa96cefaed5e1bd84e4491
Mohit <mohitsingh.it5@gmail.com>
2022-05-06 20:59:59 +0530

    Concurreny : 1116 Zero Even Odd : Removed Volatile from Sync usage | Mohit

--- Others/LC Concurrency Problems/PrintZeroEvenOdd_1116.java
+++ Others/LC Concurrency Problems/PrintZeroEvenOdd_1116.java
@@ -4,66 +4,67 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.function.IntConsumer;
 
-class ZeroEvenOdd_UsingWait_NotifyAll_synchro_volatile {
-    private int n;
-    private volatile int currState;
-    private final int ZERO, EVEN, ODD;
-
-    public ZeroEvenOdd_UsingWait_NotifyAll_synchro_volatile(int n) {
-        this.n = n;
-        this.ZERO = 0;
-        this.EVEN = 2;
-        this.ODD = 1;
-        this.currState = ZERO;
-    }
-
-    // printNumber.accept(x) outputs "x", where x is an integer.
-    public void zero(IntConsumer printNumber) throws InterruptedException {
-        for (int i = 1; i <= n; i++) {
-            synchronized (this) {
-                while (currState != ZERO)
-                    wait();
-
-                printNumber.accept(0);
-                if ((i & 1) == 1) {
-                    currState = ODD;
-                } else {
-                    currState = EVEN;
-                }
-
-                notifyAll();
-            }
-        }
-    }
-
-    public void even(IntConsumer printNumber) throws InterruptedException {
-        for (int i = 2; i <= n; i += 2) {
-            synchronized (this) {
-                while (currState != EVEN)
-                    wait();
-
-                printNumber.accept(i);
-                currState = ZERO;
-                notifyAll();
-            }
-        }
-    }
-
-    public void odd(IntConsumer printNumber) throws InterruptedException {
-        for (int i = 1; i <= n; i += 2) {
-            synchronized (this) {
-                while (currState != ODD)
-                    wait();
-
-                printNumber.accept(i);
-                currState = ZERO;
-                notifyAll();
-            }
-        }
-    }
-}
-
 public class PrintZeroEvenOdd_1116 {
+
+    class ZeroEvenOdd_UsingWait_NotifyAll_synchro_volatile {
+        private int n;
+        // private volatile int currState; no need to use volatile if using sync block
+        private int currState;
+        private final int ZERO, EVEN, ODD;
+
+        public ZeroEvenOdd_UsingWait_NotifyAll_synchro_volatile(int n) {
+            this.n = n;
+            this.ZERO = 0;
+            this.EVEN = 2;
+            this.ODD = 1;
+            this.currState = ZERO;
+        }
+
+        // printNumber.accept(x) outputs "x", where x is an integer.
+        public void zero(IntConsumer printNumber) throws InterruptedException {
+            for (int i = 1; i <= n; i++) {
+                synchronized (this) {
+                    while (currState != ZERO)
+                        wait();
+
+                    printNumber.accept(0);
+                    if ((i & 1) == 1) {
+                        currState = ODD;
+                    } else {
+                        currState = EVEN;
+                    }
+
+                    notifyAll();
+                }
+            }
+        }
+
+        public void even(IntConsumer printNumber) throws InterruptedException {
+            for (int i = 2; i <= n; i += 2) {
+                synchronized (this) {
+                    while (currState != EVEN)
+                        wait();
+
+                    printNumber.accept(i);
+                    currState = ZERO;
+                    notifyAll();
+                }
+            }
+        }
+
+        public void odd(IntConsumer printNumber) throws InterruptedException {
+            for (int i = 1; i <= n; i += 2) {
+                synchronized (this) {
+                    while (currState != ODD)
+                        wait();
+
+                    printNumber.accept(i);
+                    currState = ZERO;
+                    notifyAll();
+                }
+            }
+        }
+    }
 
     class ZeroEvenOdd_UsingReentrantLock_volatile {
         private int n;
@@ -140,48 +141,48 @@ public class PrintZeroEvenOdd_1116 {
         }
     }
 
-}
+    class ZeroEvenOdd_Using_Semaphores {
+        private int n;
+        private Semaphore zero, even, odd;
 
-class ZeroEvenOdd_Using_Semaphores {
-    private int n;
-    private Semaphore zero, even, odd;
+        public ZeroEvenOdd_Using_Semaphores(int n) {
+            this.n = n;
+            this.zero = new Semaphore(1);
+            this.even = new Semaphore(0);
+            this.odd = new Semaphore(0);
+        }
 
-    public ZeroEvenOdd_Using_Semaphores(int n) {
-        this.n = n;
-        this.zero = new Semaphore(1);
-        this.even = new Semaphore(0);
-        this.odd = new Semaphore(0);
-    }
+        // printNumber.accept(x) outputs "x", where x is an integer.
+        public void zero(IntConsumer printNumber) throws InterruptedException {
+            for (int i = 1; i <= n; i++) {
+                zero.acquire();
 
-    // printNumber.accept(x) outputs "x", where x is an integer.
-    public void zero(IntConsumer printNumber) throws InterruptedException {
-        for (int i = 1; i <= n; i++) {
-            zero.acquire();
+                printNumber.accept(0);
+                if (i % 2 == 0) {
+                    even.release();
+                } else {
+                    odd.release();
+                }
 
-            printNumber.accept(0);
-            if (i % 2 == 0) {
-                even.release();
-            } else {
-                odd.release();
             }
-
         }
-    }
 
-    public void even(IntConsumer printNumber) throws InterruptedException {
-        for (int i = 2; i <= n; i += 2) {
-            even.acquire();
-            printNumber.accept(i);
-            zero.release();
+        public void even(IntConsumer printNumber) throws InterruptedException {
+            for (int i = 2; i <= n; i += 2) {
+                even.acquire();
+                printNumber.accept(i);
+                zero.release();
+            }
         }
-    }
 
-    public void odd(IntConsumer printNumber) throws InterruptedException {
-        for (int i = 1; i <= n; i += 2) {
-            odd.acquire();
-            printNumber.accept(i);
-            zero.release();
+        public void odd(IntConsumer printNumber) throws InterruptedException {
+            for (int i = 1; i <= n; i += 2) {
+                odd.acquire();
+                printNumber.accept(i);
+                zero.release();
+            }
         }
+
     }
 
 }
