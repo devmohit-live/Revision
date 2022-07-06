d007bc2e0520c7a829e77ca52da99d2520dcf292
Mohit <mohitsingh.it5@gmail.com>
2022-07-06 21:13:06 +0530

    Word Ladder : 126 | Mohit

--- Others/Questions/AMZ/In/WordLadder.java
+++ Others/Questions/AMZ/In/WordLadder.java
@@ -1,5 +1,155 @@
 public class WordLadder {
-    // WOrd Ladder II : TLE
+
+    // Word Ladder 1 : LC 127
+    // Treat every word as node and every transformation as an edge or wt 1;
+    // shortest path in terms of edges: bfs
+
+    // Space: O(n) Haset, Q: O(l*26*N) : O(l*n)
+    // Time: set: O(n) + ( possible word creation: O(26*l*l*) * N(we are adding only
+    // elements in q which exists in wordlist) : O(n) + O(N*26*l*l) => O(N*l*l)
+
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        // return appraoch1(beginWord, endWord, wordList);
+        return approach2(beginWord, endWord, wordList);
+    }
+
+    // Appraoah 2 : Build connections on the way when required: if no connecting
+    // nodes are possible then return 0; : no need to build a graph in advance
+
+    private int approach2(String beginWord, String endWord, List<String> wordList) {
+        Set<String> words = new HashSet<>(wordList);
+        words.add(beginWord);
+
+        if (beginWord.length() != endWord.length())
+            return 0;
+        int len = beginWord.length();
+        if (!words.contains(endWord))
+            return 0;
+
+        LinkedList<String> q = new LinkedList<>();
+        q.addLast(beginWord);
+
+        words.remove(beginWord); // will act as visited : check wheather we have any next word in list to jump on
+                                 // to or not
+        int level = 1;
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            while (size-- > 0) {
+                String rm = q.removeFirst();
+                if (rm.equals(endWord)) {
+                    return level;
+                }
+                // build new nodes if possible for connection only from the current node
+                for (String nbr : getNextNodes(words, rm, len)) {
+                    q.addLast(nbr);
+                    words.remove(nbr);
+                }
+            }
+
+            level++;
+        }
+        return 0;
+    }
+
+    private Set<String> getNextNodes(Set<String> words, String curr, int len) {
+        // only build new connecting nodes from the curr node not from all words in
+        // wordlist set
+        Set<String> ans = new HashSet<>();
+        char[] arr = curr.toCharArray();
+        for (int i = 0; i < len; i++) {
+            for (char ch = 'a'; ch <= 'z'; ch++) {
+                char tmp = arr[i];
+                if (tmp == ch)
+                    continue;
+
+                arr[i] = ch;
+                String transformed = new String(arr);
+                if (words.contains(transformed)) {
+                    ans.add(transformed);
+                }
+                arr[i] = tmp;
+            }
+        }
+
+        return ans;
+    }
+
+    Map<String, Set<String>> graph;
+
+    // Approach 1: was failing on direct retiurning level so used label;
+    private int appraoch1(String beginWord, String endWord, List<String> wordList) {
+        Set<String> words = new HashSet<>(wordList);
+        words.add(beginWord);
+
+        if (beginWord.length() != endWord.length())
+            return 0;
+        if (!words.contains(endWord))
+            return 0;
+
+        graph = new HashMap<>();
+        buildGraph(words);
+        System.out.println(graph);
+
+        int level = 1;
+        // bfs
+        LinkedList<String> q = new LinkedList<>();
+        q.addLast(beginWord);
+        Set<String> vis = new HashSet<>();
+        vis.add(beginWord);
+        boolean isPossible = false;
+
+        bfs: while (!q.isEmpty()) {
+            int size = q.size();
+            while (size-- > 0) {
+                String rm = q.removeFirst();
+                if (rm.equals(endWord)) {
+                    isPossible = true;
+                    break bfs;
+                    // return level;
+                }
+
+                if (graph.get(rm).size() == 0)
+                    return 0; // no immediate neighbours
+
+                // or take 2 q's : parent and child at every level check if child q is empty
+
+                for (String nbr : graph.get(rm)) {
+                    if (!vis.contains(nbr)) {
+                        vis.add(nbr);
+                        q.addLast(nbr);
+                    }
+                }
+            }
+
+            level++;
+        }
+
+        return isPossible ? level : 0;
+    }
+
+    private void buildGraph(Set<String> words) {
+        for (String word : words) {
+            graph.put(word, new HashSet<String>());
+            char arr[] = word.toCharArray();
+            for (int idx = 0; idx < arr.length; idx++) {
+                for (char ch = 'a'; ch <= 'z'; ch++) {
+                    char tmp = arr[idx];
+                    if (tmp == ch)
+                        continue;
+
+                    arr[idx] = ch;
+                    String transformed = new String(arr);
+                    if (words.contains(transformed))
+                        graph.get(word).add(transformed);
+
+                    arr[idx] = tmp;
+                }
+            } // idx
+        } // words
+    }
+
+    // Word Ladder II : TLE
     // DFs cause tle , and I am not able to figure it out how to form multiple
     // ansers using bfs
     // as i will get to know whether this path forms an answer or not at last level
