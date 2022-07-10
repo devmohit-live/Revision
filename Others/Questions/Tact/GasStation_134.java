e1cc4393ea7108c67bc95fcd6cde8da661177c35
Mohit <mohitsingh.it5@gmail.com>
2022-07-10 10:53:08 +0530

    Tact: Gas Station  | Mohit

--- Others/Questions/Tact/GasStation_134.java
+++ Others/Questions/Tact/GasStation_134.java
@@ -1,3 +1,17 @@
 public class GasStation_134 {
-    
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int n = gas.length, def = 0, tank = 0, net = 0, ans = 0;
+        for (int i = 0; i < n; i++) {
+            tank += gas[i] - cost[i];
+            net += gas[i] - cost[i]; // tracks if it is possible to caomplete the journey
+            if (tank < 0) {
+                ans = i + 1;
+                tank = 0;
+            }
+
+        }
+
+        return net < 0 ? -1 : ans;
+
+    }
 }
