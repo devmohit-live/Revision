c3e5c25eb162dba617a6ccb67cfec6a28f15e2d6
Mohit <mohitsingh.it5@gmail.com>
2022-09-29 14:41:54 +0530

    Java : Static Question Commented | Mohit

--- Java/Static/StaticQuestion.java
+++ Java/Static/StaticQuestion.java
@@ -1,19 +1,30 @@
-public class StaticQuestion{
-    static {
+public class StaticQuestion {
+
+    // Ans : bdac100
+
+    // //Normal static blocks : line wise -> main -> calss called in main's staic
+    // block linewise
+
+    public static void main(String[] args) {
+        System.out.println("a");// 3
+        System.out.println(Class1.i);// 4
+    }
+
+    static { // 1
         System.out.println("b");
     }
-	public static void main(String[] args) {
-		System.out.println("a");
-		System.out.println(Class1.i);
-	}
+
+    static {// 2
+        System.out.println("d");
+    }
 }
 
-class Class1{
+class Class1 {
     static int i;
-    
-    static{
-       System.out.println("c");
-        i= 100;
+
+    static {
+        System.out.println("c");
+        i = 100;
     }
-   
+
 }
