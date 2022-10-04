8f2965f4ef022e6dcfd6803843152747e3a792ae
Mohit <mohitsingh.it5@gmail.com>
2022-10-04 21:05:47 +0530

    Add String : 415 | Mohit

---
+++ Others/Daily Leetcode, Contest/AddString_415.java
@@ -0,0 +1,15 @@
+public class AddString_415 {
+    public String addStrings(String num1, String num2) {
+        StringBuilder sb = new StringBuilder();
+        int carry = 0, i = num1.length() - 1, j = num2.length() - 1;
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int x = i < 0 ? 0 : num1.charAt(i) - '0';
+            int y = j < 0 ? 0 : num2.charAt(j) - '0';
+            sb.append((x + y + carry) % 10);
+            carry = (x + y + carry) / 10;
+            i--;
+            j--;
+        }
+        return sb.reverse().toString();
+    }
+}
