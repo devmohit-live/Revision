c4f7dc26346e75e94d9b865344af951e7bd81dd7
Mohit <mohitsingh.it5@gmail.com>
2022-10-09 15:42:22 +0530

    Update | Mohit

--- Others/Daily Leetcode, Contest/TwoSumIV_653.java
+++ Others/Daily Leetcode, Contest/TwoSumIV_653.java
@@ -26,4 +26,19 @@ public class TwoSumIV_653 {
         inorder(root.right, nodes);
 
     }
+
+    // Using HashSet
+    public boolean findTarget2(TreeNode root, int k) {
+        HashSet<Integer> set = new HashSet<>();
+        return dfs(root, set, k);
+    }
+
+    public boolean dfs(TreeNode root, HashSet<Integer> set, int k) {
+        if (root == null)
+            return false;
+        if (set.contains(k - root.val))
+            return true;
+        set.add(root.val);
+        return dfs(root.left, set, k) || dfs(root.right, set, k);
+    }
 }
