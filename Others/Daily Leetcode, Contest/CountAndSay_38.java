091c08dbf347476191216212ba16cbe6ace94d37
Mohit <mohitsingh.it5@gmail.com>
2022-10-18 12:49:33 +0530

    Count and Say Lc 38 | Mohit

---
+++ Others/Daily Leetcode, Contest/CountAndSay_38.java
@@ -0,0 +1,38 @@
+public class CountAndSay_38 {
+    public String countAndSay(int num) {
+        if(num <= 0) return "";
+        String ans = "1";
+        if(num==1) return ans;
+        
+        int c = 1;
+        // System.out.println(ans);
+        
+        //O(num*n)
+        while(c++<num){//O(num)
+            ans = countFreqAndCreateString(ans);//O(n)
+            // System.out.println(ans);
+        }
+        return ans;
+    }
+    
+    //O(3n) => O(n)
+    private String countFreqAndCreateString(String s){
+        // System.out.println("For "+s);
+        char[] arr = s.toCharArray(); //O(n)
+        int n = s.length();
+        StringBuilder sb = new StringBuilder();
+        int idx = 0;
+        //O(n)
+        while(idx<n){
+            int count = 1;
+            while(idx+1<n && arr[idx] == arr[idx+1]){
+                count++;
+                idx++;
+            }
+            sb.append(count+""+arr[idx]);//O(2)
+            // System.out.println("Sb "+sb.toString());
+            idx++;
+        }
+        return sb.toString(); //O(n)
+    }
+}
