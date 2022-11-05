440b93903d019fe6cd36a9751c4c797bff2a7a06
Mohit <mohitsingh.it5@gmail.com>
2022-11-06 01:04:36 +0530

    Update | Mohit

--- Others/Questions/SDE Sheet/NextGreater.java
+++ Others/Questions/SDE Sheet/NextGreater.java
@@ -20,31 +20,29 @@ public class NextGreater {
     }
 
     // ng1
-    
 
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         int[] ans = new int[nums1.length];
-        Map<Integer, Integer> val2Idx = new HashMap<>(); // for nums1
         int n = nums1.length, m = nums2.length, idx = 0;
         if (n == 0)
             return ans;
 
         Arrays.fill(ans, -1);
-
+        Map<Integer, Integer> val2Idx = new HashMap<>();
         for (int val : nums1)
             val2Idx.put(val, idx++);
 
-        // ngor for nums2
+        // ngor have to run in nums2 array for nums1 values that are present in nums2
         Stack<Integer> st = new Stack<>();
         st.push(-1); // ref
         for (int i = 0; i < m; i++) {
             while (st.peek() != -1 && nums2[st.peek()] < nums2[i]) {
-                int ref = nums2[st.peek()];
+                int ref = nums2[st.peek()]; // number in nums2
 
-                // setting the ngor
+                // setting the ngor only if this number was present in nums1
                 if (val2Idx.containsKey(ref)) {
-                    idx = val2Idx.get(ref);
-                    ans[idx] = nums2[i];
+                    idx = val2Idx.get(ref); // idx of this number in nums1
+                    ans[idx] = nums2[i]; // it's ngor'
                 }
 
                 st.pop();
@@ -55,5 +53,6 @@ public class NextGreater {
         }
 
         return ans;
+
     }
 }
