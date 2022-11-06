c0978f51ce0549587d614074ea825833a2295375
Mohit <mohitsingh.it5@gmail.com>
2022-11-06 12:29:02 +0530

    Comstructor Callings in Inheritance | Mohit

--- Java/Polymorphisms/ConstructorCalling.java
+++ Java/Polymorphisms/ConstructorCalling.java
@@ -1,3 +1,46 @@
 public class ConstructorCalling {
-    
+    public static void main(String[] args) {
+
+        // Summary : All upper constructors will be executed for till we reach the point
+        // for which memory is assigned(new object is created)
+        // irrespective of refrence
+
+        System.out.print(" A a = new A(); ");
+        A a = new A();
+        System.out.println();
+        System.out.print(" B b = new B(); ");
+        B b = new B();
+        System.out.println();
+        System.out.print(" C c = new C(); ");
+        C c = new C();
+        System.out.println();
+        System.out.print(" A x = new B(); ");
+        A x = new B();
+        System.out.println();
+        System.out.print(" A y = new C(); ");
+        A y = new C();
+        System.out.println();
+        System.out.print(" B z = new C(); ");
+        B z = new C();
+        System.out.println();
+
+    }
+}
+
+class A {
+    A() {
+        System.out.print("This is A's Constructor ");
+    }
+}
+
+class B extends A {
+    B() {
+        System.out.print("This is B's Constructor ");
+    }
+}
+
+class C extends B {
+    C() {
+        System.out.println("This is C's Constructor");
+    }
 }
